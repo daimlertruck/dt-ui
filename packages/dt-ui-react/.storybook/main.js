88181b129a8d91fad955e45c37156351c57b3f63
Cátia Raminhos <catia.raminhos@daimler.com>
2023-08-24 18:16:38 +0100

    docs: setup directory for standalone docs

--- packages/dt-ui-react/.storybook/main.js
+++ packages/dt-ui-react/.storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
-  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx|mdx)', '../themes/*.stories.mdx'],
+  stories: [
+    '../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../themes/*.stories.mdx',
+    '../docs/**/*.stories.mdx',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
