5d7de5e70818e0a04bc50e71c046228027bfcb6e
pereia1 <ana.m.pereira@daimler.com>
2023-09-06 12:03:19 +0100

    feat: add MoreHorizontal icon

--- packages/dt-ui-react/core/assets/svgs/index.ts
+++ packages/dt-ui-react/core/assets/svgs/index.ts
@@ -35,3 +35,4 @@ export { default as OutlinedArrowDropUp } from './OutlinedArrowDropUp';
 export { default as NotFound } from './NotFound';
 export { default as NoDataIcon } from './NoData';
 export { default as RemoveCircleOutline } from './RemoveCircleOutline';
+export { default as MoreHorizontalIcon } from './MoreHorizontal';
