3f8ae5117c904aa7a016c92fcf27ada2f81cbc88
Masoud Soleymani <msoleym@tbdir.net>
2023-09-20 15:15:42 +0100

    fix: remove page components
    Page-header and Page-container are not sharable components
    therefore they moved to API portal project.

--- packages/dt-ui-react/components/page/page-container/PageContainer.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { PageContainer } from './PageContainer';
-
-export default {
-  title: 'Data Display/PageContainer',
-  component: PageContainer,
-};
-
-export const Default = () => <PageContainer>Page Content</PageContainer>;
