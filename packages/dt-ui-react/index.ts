5e1d1576f4cac34fdd82dee6aa8a438806c95ec6
Diogo Costa <diogo.costa@daimlertruck.com>
2023-11-28 09:25:37 +0000

    feat: export useMedia hook to be reusable on apps

--- packages/dt-ui-react/index.ts
+++ packages/dt-ui-react/index.ts
@@ -50,6 +50,9 @@ export { debounce, useClickOutside } from './utils/index';
 export { ListStyleType } from './utils/constants';
 export * from './utils/testingUtils';
 
+//hooks
+export * from './hooks';
+
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends CustomTheme {}
