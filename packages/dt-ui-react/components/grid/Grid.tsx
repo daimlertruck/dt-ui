0f9b423f0774bd313102a5748b8e9444737c5ecc
Pereira <joel.pereira@daimler.com>
2023-11-28 10:01:55 +0000

    feat: extend div props in the grid

--- packages/dt-ui-react/components/grid/Grid.tsx
+++ packages/dt-ui-react/components/grid/Grid.tsx
@@ -2,15 +2,21 @@ import { BaseProps } from '../../types';
 
 import { GridStyled } from './Grid.styled';
 
-interface GridProps extends BaseProps {
+interface GridProps extends BaseProps, React.HTMLAttributes<HTMLDivElement> {
   gridTemplateColumns?: string;
 }
 
-export const Grid = ({ children, style, gridTemplateColumns }: GridProps) => (
+export const Grid = ({
+  children,
+  style,
+  gridTemplateColumns,
+  ...rest
+}: GridProps) => (
   <GridStyled
     data-testid='grid'
     gridTemplateColumns={gridTemplateColumns}
     style={style}
+    {...rest}
   >
     {children}
   </GridStyled>
