42dd5490bc4188b84ecaa8a45a2e1d19db8d1e24
dt-ui <dt-ui@daimlertruck.com>
2024-01-29 10:01:41 +0000

    chore: delete PR preview

--- PR-125/assets/Box.stories-8XjHnc4w.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{B as s}from"./index-H1EXaXUo.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Box.styled-oB9eueTz.js";import"./emotion-styled.browser.esm-Scfyu_ZY.js";import"./extends-E30VzY7q.js";const B={title:"Data Display/Box",component:s,argTypes:{element:{control:"text"}}},a=({...p})=>o.jsxs(s,{...p,children:[o.jsx("p",{children:"Content 1"}),o.jsx("p",{children:"Content 2"})]}),t=a.bind({});t.args={};var r,e,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`({
-  ...props
-}) => {
-  return <Box {...props}>
-      <p>Content 1</p>
-      <p>Content 2</p>
-    </Box>;
-}`,...(n=(e=t.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,B as default};
