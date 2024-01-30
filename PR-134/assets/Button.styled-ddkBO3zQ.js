04b5320e0459d6085651c93b3e90c567c58d8f17
dt-ui <dt-ui@daimlertruck.com>
2024-01-30 18:52:18 +0000

    chore: update and publish Storybook

---
+++ PR-134/assets/Button.styled-ddkBO3zQ.js
@@ -0,0 +1,117 @@
+import{n as t}from"./emotion-styled.browser.esm-Scfyu_ZY.js";const n=r=>({primary:`
+    color: ${r.palette.primary};
+    background-color: ${r.colors.white};
+    border: 1px solid ${r.palette.primary};
+    &:hover {
+      color: ${r.palette.primaryDark_300};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.primaryDark_300};
+    }
+    &:active {
+      color: ${r.palette.primaryDark_300};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.primaryDark_300};
+    }
+    &:disabled {
+      cursor: not-allowed;
+      color: ${r.palette.primaryLight_300};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.primaryLight_300};
+    }
+  `,error:`
+    color: ${r.palette.error};
+    background-color: ${r.colors.white};
+    border: 1px solid ${r.palette.error};
+    &:hover {
+      color: ${r.palette.errorDark_300};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.errorDark_300};
+    }
+    &:active {
+      color: ${r.palette.errorDark_300};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.errorDark_300};
+    }
+    &:disabled {
+      cursor: not-allowed;
+      color: ${r.palette.errorLight_300};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.errorLight_300};
+    }
+  `,neutral:`
+    color: ${r.palette.neutralDark_400};
+    background-color: ${r.colors.white};
+    border: 1px solid ${r.palette.neutralDark_100};
+    &:hover {
+      color: ${r.palette.neutralDark_700};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.neutralDark_200};
+    }
+    &:active {
+      color: ${r.palette.neutralDark_700};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.neutralDark_200};
+    }
+    &:disabled {
+      cursor: not-allowed;
+      color: ${r.palette.neutralDark_100};
+      background-color: ${r.colors.white};
+      border: 1px solid ${r.palette.neutralDark_100};
+    }
+  `}),c=(r,o,a)=>({solid:`
+      border: ${o.spacing.none};
+      color: ${o.colors.white};
+      background-color: ${o.palette[a]};
+      
+      &:hover {
+        color: ${o.colors.white};
+        background-color: ${o.palette[`${a}Dark_300`]};
+      }
+
+      &:disabled {
+        cursor: not-allowed;
+        opacity: 38%;
+      }
+    `,outlined:n(o)[a],text:`
+      border: none;
+      color: ${o.palette[a]};
+      background-color: 'transparent';
+      
+      &:hover {
+        color: ${o.palette[`${a}Dark_300`]};
+        background-color: 'transparent';
+      }
+      
+      &:disabled {
+        cursor: not-allowed;
+        color: ${o.palette.neutralLight_100};
+      }
+    `})[r],i=(r,o)=>({large:`
+      ${o.fontStyles.button1}
+      padding: ${o.spacing["2xs"]} ${o.spacing.s};
+    `,medium:`
+      ${o.fontStyles.button2}
+      padding: ${o.spacing["3xs"]} ${o.spacing.xs};
+    `,small:`
+      ${o.fontStyles.button2}
+      padding: ${o.spacing["4xs"]} ${o.spacing["2xs"]};
+    `})[r],d=t.button`
+  ${({theme:r,variant:o,color:a,size:l})=>`
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    width: 100%;
+    min-width: 80px;
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+    align-items: center;
+    gap: ${r.spacing["4xs"]};
+    border-radius: ${r.shape.button};
+    
+    ${i(l,r)}
+    ${c(o,r,a)}
+
+    @media only screen and (min-width: ${r.breakpoints.s}) {
+      width: auto;
+    }
+  `}
+`;export{d as B};
