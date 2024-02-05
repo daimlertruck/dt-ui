35e11ab4e13ee37d108d9934ba63f53db7b6a624
dt-ui <dt-ui@daimlertruck.com>
2024-02-05 16:30:55 +0000

    chore: update and publish Storybook

---
+++ PR-124/assets/LabelFied.styled-i3rg1hk1.js
@@ -0,0 +1,13 @@
+import{n as o}from"./emotion-styled.browser.esm-Scfyu_ZY.js";const a=o.label`
+  ${({theme:t})=>t.fontStyles.pSmall}
+
+  color: ${({theme:t,disabled:e})=>e?t.palette.content.disabled:t.palette.content.body};
+  cursor: inherit;
+  font-size: ${({isActive:t})=>t?".75rem":"1rem"};
+  left: 15px;
+  top: 15px;
+  position: absolute;
+
+  transform: ${({isActive:t})=>t?"translateY(-45%)":"translateY(0)"};
+  transition: transform 150ms ease-out, font-size 150ms ease-out;
+`;export{a as L};
