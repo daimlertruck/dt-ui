190fdb0f8a84fd71f11676eb60893226ea548e0f
dt-ui <dt-ui@daimlertruck.com>
2024-02-05 12:58:00 +0000

    chore: update and publish Storybook

---
+++ PR-140/assets/Tooltip.stories-GD6ZYvpC.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{n as h}from"./emotion-styled.browser.esm-Scfyu_ZY.js";import"./Box.styled-oB9eueTz.js";import"./Typography-GeESgPPh.js";import"./Spinner.styled-2Rn7jPaQ.js";import"./Button.styled-fGz7p1PP.js";import{I as g}from"./IconButton--b11-rHN.js";import"./index-lb1Yyg-x.js";import{T as r,a as s,O as a}from"./Tooltip-X3IACSlU.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-E30VzY7q.js";import"./typography--O4Wr7D9.js";import"./emotion-react.browser.esm-7R29exhq.js";import"./IconButton.styled-39rpUtWL.js";import"./index-XRl7aEt-.js";import"./index-5tMk2lpP.js";import"./util-p6oSlpjm.js";import"./Portal-EY94Zi_-.js";import"./index-jmm5gWkb.js";import"./z-index-zwCKGdcf.js";const u=e=>o.jsxs("svg",{fill:"none",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg",...e,children:[o.jsxs("g",{clipPath:"url(#clip0_287_3834)",children:[o.jsx("path",{d:"M8 9H16V19H8V9Z",fill:"currentColor",opacity:"0.3"}),o.jsx("path",{d:"M15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5ZM6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9Z",fill:"currentColor"})]}),o.jsx("defs",{children:o.jsx("clipPath",{id:"clip0_287_3834",children:o.jsx("rect",{fill:"white",height:"24",width:"24"})})})]}),x=h.div`
+  text-align: center;
+  margin-top: 100px;
+`,L={component:r,title:"Data Display/Tooltip",argTypes:{background:{options:s,control:{type:"radio"}},direction:{options:a,control:{type:"select"}},children:{table:{disable:!0}}},render:({children:e,hideDelay:c,background:p,direction:d,message:m})=>o.jsx(x,{children:o.jsxs(r,{hideDelay:c,children:[e,o.jsx(r.Content,{background:p,direction:d,children:m})]})})},t={args:{hideDelay:0,children:o.jsx(g,{color:"error",dataTestId:"delete-id-button-",isDisabled:!1,onClick:()=>console.log("log"),children:o.jsx(u,{})}),direction:a.Top,background:s.Full,message:"Delete"}};var i,n,l;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    hideDelay: 0,
+    children: <IconButton color='error' dataTestId='delete-id-button-' isDisabled={false} onClick={() => console.log('log')}>
+        <DeleteIcon />
+      </IconButton>,
+    direction: OTooltipDirection.Top,
+    background: OTooltipBackground.Full,
+    message: 'Delete'
+  }
+}`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const P=["Template"];export{t as Template,P as __namedExportsOrder,L as default};
