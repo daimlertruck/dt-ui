4f373fa0fe18e035e9bdf84dacd2635b77ddf431
Pereira <joel.pereira@daimler.com>
2024-02-05 11:03:41 +0000

    refactor: update tooltip story

--- packages/react-packages/tooltip/src/Tooltip.stories.tsx
+++ packages/react-packages/tooltip/src/Tooltip.stories.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { IconButton } from '../../../dt-ui-react/components/buttons';
 import { DeleteIcon } from '../../../dt-ui-react/core';
@@ -9,54 +9,63 @@ import { TooltipContentProps } from './components/content';
 import { OTooltipBackground, OTooltipDirection } from './constants';
 import Tooltip from './Tooltip';
 
-export default {
-  title: 'Data Display/Tooltip',
-  component: Tooltip,
-};
-
 const StyledContainer = styled.div`
   text-align: center;
   margin-top: 100px;
 `;
 
-const Template: Story<{
-  container: TooltipContainerProps;
-  content: TooltipContentProps;
-}> = ({
-  container: { children, hideDelay },
-  content: { children: contentChildren, direction, background },
-}) => {
-  return (
-    <StyledContainer>
-      <Tooltip hideDelay={hideDelay}>
-        {children}
-        <Tooltip.Content background={background} direction={direction}>
-          {contentChildren}
-        </Tooltip.Content>
-      </Tooltip>
-    </StyledContainer>
-  );
+type TooltipProps = TooltipContainerProps &
+  TooltipContentProps & { message?: string };
+
+const meta: Meta<TooltipProps> = {
+  component: Tooltip,
+  title: 'Data Display/Tooltip',
+  argTypes: {
+    background: {
+      options: OTooltipBackground,
+      control: { type: 'radio' },
+    },
+    direction: {
+      options: OTooltipDirection,
+      control: { type: 'select' },
+    },
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  render: ({ children, hideDelay, background, direction, message }) => {
+    return (
+      <StyledContainer>
+        <Tooltip hideDelay={hideDelay}>
+          {children}
+          <Tooltip.Content background={background} direction={direction}>
+            {message}
+          </Tooltip.Content>
+        </Tooltip>
+      </StyledContainer>
+    );
+  },
 };
 
-export const Default = Template.bind({});
+export default meta;
 
-Default.args = {
-  container: {
+export const Template: StoryObj<TooltipProps> = {
+  args: {
     hideDelay: 0,
     children: (
       <IconButton
         color='error'
-        dataTestId='delete-access-id-button-'
+        dataTestId='delete-id-button-'
         isDisabled={false}
-        onClick={() => console.log('hey')}
+        onClick={() => console.log('log')}
       >
         <DeleteIcon />
       </IconButton>
     ),
-  },
-  content: {
     direction: OTooltipDirection.Top,
     background: OTooltipBackground.Full,
-    children: 'Delete',
+    message: 'Delete',
   },
 };
