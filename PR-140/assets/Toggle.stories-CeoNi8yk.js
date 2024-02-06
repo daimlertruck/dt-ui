cf42695f3a1257cc582e6c4ce6d3c100a1cbfe79
dt-ui <dt-ui@daimlertruck.com>
2024-02-06 14:21:32 +0000

    chore: delete PR preview

--- PR-140/assets/Toggle.stories-CeoNi8yk.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-vNq4Oc-g.js";import{T as o}from"./Toggle-Q2LnH90j.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Typography-GeESgPPh.js";import"./typography--O4Wr7D9.js";import"./index-lb1Yyg-x.js";import"./emotion-react.browser.esm-7R29exhq.js";import"./emotion-styled.browser.esm-Scfyu_ZY.js";import"./extends-E30VzY7q.js";import"./index-XRl7aEt-.js";import"./index-5tMk2lpP.js";import"./util-p6oSlpjm.js";const k={title:"Data Display/form/Toggle",component:o},i=({label:l,...p})=>r.jsx(o,{...p,children:r.jsx(o.Label,{children:l})}),e=i.bind({});e.args={isChecked:!1,isDisabled:!1,label:"",checkedColor:"success",uncheckedColor:"secondary"};var s,t,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`({
-  label,
-  ...props
-}) => {
-  return <Toggle {...props}>
-      <Toggle.Label>{label}</Toggle.Label>
-    </Toggle>;
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,k as default};
