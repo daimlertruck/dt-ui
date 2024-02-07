0715e37b4d532f0cdaac0e4987850458db5b6327
dt-ui <dt-ui@daimlertruck.com>
2024-02-07 11:45:41 +0000

    chore: update and publish Storybook

---
+++ PR-147/assets/ProgressBar.styled-jrxdW072.js
@@ -0,0 +1,50 @@
+import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{n as t}from"./emotion-styled.browser.esm-QTPHyGVR.js";const d=r=>o.jsx("svg",{fill:"currentColor",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg",...r,children:o.jsx("path",{d:"M12 0C5.376 0 0 5.376 0 12C0 18.624 5.376 24 12 24C18.624 24 24 18.624 24 12C24 5.376 18.624 0 12 0ZM12 21.6C6.708 21.6 2.4 17.292 2.4 12C2.4 6.708 6.708 2.4 12 2.4C17.292 2.4 21.6 6.708 21.6 12C21.6 17.292 17.292 21.6 12 21.6ZM17.508 6.696L9.6 14.604L6.492 11.508L4.8 13.2L9.6 18L19.2 8.4L17.508 6.696Z",fill:"currentColor"})}),p=r=>o.jsx("svg",{fill:"none",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg",...r,children:o.jsx("path",{d:"M11.988 0C5.364 0 0 5.376 0 12C0 18.624 5.364 24 11.988 24C18.624 24 24 18.624 24 12C24 5.376 18.624 0 11.988 0ZM12 21.6C6.696 21.6 2.4 17.304 2.4 12C2.4 6.696 6.696 2.4 12 2.4C17.304 2.4 21.6 6.696 21.6 12C21.6 17.304 17.304 21.6 12 21.6ZM10.8 15.6H13.2V18H10.8V15.6ZM10.8 6H13.2V13.2H10.8V6Z",fill:"currentColor"})}),l={small:"4px",large:"8px"},s=r=>({active:{background:r.palette.informative.default,helper:r.palette.content.secondary,percentage:r.palette.content.body,icon:r.palette.content.body},success:{background:r.palette.success.default,helper:r.palette.content.secondary,percentage:r.palette.content.body,icon:r.palette.success.default},error:{background:r.palette.error.default,helper:r.palette.error.default,percentage:r.palette.error.default,icon:r.palette.error.default}}),g=t.div`
+  ${({theme:r})=>`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: ${r.spacing["5xs"]};
+    min-width: 48px;
+  `}
+`,u=t.div`
+  ${({theme:r})=>`
+    display: grid;
+    grid-template-columns: minmax(48px, 1fr) 28px 16px;
+    align-items: center;
+    gap: ${r.spacing["5xs"]};
+    width: 100%;
+    height: 16px;
+  `}
+`,x=t.div`
+  ${({theme:r,size:e,state:a,progress:n})=>`
+    width: ${n}%;
+    height: ${l[e]};
+    background-color: ${s(r)[a].background};
+    transition: width 0.2s ease-in-out;
+    border-radius: ${r.radius.none} ${r.shape.progressbar} ${r.shape.progressbar} ${r.radius.none}; 
+  `};
+`,C=t.div`
+  ${({theme:r})=>`
+    width: 100%;
+    background-color: ${r.palette.secondary.medium};
+    border-radius: ${r.shape.progressbar};
+    overflow: hidden;
+  `};
+`,$=t.p`
+  ${({theme:r,state:e})=>`
+    ${r.fontStyles.body3}
+    color: ${s(r)[e].helper};
+  `};
+`,f=t.div`
+  ${({theme:r,state:e})=>`
+    ${r.fontStyles.body3}
+    text-align: center;
+    color: ${s(r)[e].percentage};
+  `};
+`,y=t.div`
+  ${({theme:r,state:e})=>`
+    height: 16px;
+    color: ${s(r)[e].icon};
+    line-height: 16px;
+  `};
+`;export{d as C,p as E,g as P,u as a,C as b,x as c,$ as d,f as e,y as f};
---
+++ PR-147/assets/Table.stories-McOn2wc3.js
@@ -0,0 +1,45 @@
+import{j as a}from"./jsx-runtime-vNq4Oc-g.js";import{A as f}from"./Avatar-dJiJSSGl.js";import{A as C}from"./Avatar.styled-5c-I8N7j.js";import{T as R}from"./Tag-S0pXlfY5.js";import{T as D,a as F,b as $,R as A,C as k,c as E}from"./Table.styled-HJRK4vTh.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Tooltip-Kt2lY6UW.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";import"./index-P2lXlfzc.js";import"./index-jmm5gWkb.js";import"./index-XRl7aEt-.js";import"./util-p6oSlpjm.js";import"./index-5tMk2lpP.js";import"./emotion-react.browser.esm-OUs2k2GK.js";import"./Portal-EY94Zi_-.js";import"./z-index-zwCKGdcf.js";import"./MultiSelectProvider-vrdgqO57.js";import"./Close-fCiS4iy8.js";const c=({children:n,dataTestId:e,isFixed:r=!1})=>a.jsx(D,{"data-testid":e??"table",isFixed:r,children:n}),y=({children:n,hasFixedHeader:e=!1})=>a.jsx(F,{hasFixedHeader:e,children:n}),j=({children:n})=>a.jsx($,{children:n}),i=({children:n,onClick:e})=>{const r=!!e;return a.jsx(A,{...e&&{onClick:e},selectableRow:r,children:n})},g=({children:n,textAlign:e="left"})=>a.jsx(k,{textAlign:e,children:n}),w=({children:n,columnWidth:e,dataLabel:r,textAlign:o="left"})=>a.jsx(E,{columnWidth:e,"data-label":r,textAlign:o,children:n}),oe={title:"Data Display/Table",component:c},l=["Name","Email","Role","Status"],d=[["Thomas Morse","thomas.morse@myhugecompanynamem.com","Operator","yellow"],["Hanna Apple","hanna.apple@companysample.com","Admin","green"],["Marcus Klein","marcus.klein@companysample.com","Operator","green"],["Rick Thor","rick.thor@companysample.com","Admin","grey"]],S={Name:"200px",Status:"120px"},v=n=>a.jsxs("div",{style:{display:"flex"},children:[a.jsx(f,{title:n,type:C.TeamMember}),n]}),B=n=>a.jsx(R,{border:"rounded",color:n,size:"small",children:{green:"ACTIVE",yellow:"PENDING",grey:"DEACTIVATED"}[n]}),H=(n,e)=>({Name:v(e),Status:B(e),Email:e,Role:e})[l[n]],N=({isFixed:n})=>a.jsxs(c,{isFixed:n,children:[a.jsx(y,{children:a.jsx(i,{children:l.map(e=>a.jsx(g,{children:e},`column-header-${e}`))})}),a.jsx(j,{children:d.map(e=>a.jsx(i,{children:e.map((r,o)=>a.jsx(w,{columnWidth:S[l[o]],dataLabel:l[o],children:H(o,r)},`column-${l[o]}-${r}`))},e.toString()))})]}),t=N.bind({});t.args={isFixed:!1};const W=({isFixed:n,hasFixedHeader:e})=>a.jsx("div",{style:{height:"200px",overflow:"auto"},children:a.jsxs(c,{isFixed:n,children:[a.jsx(y,{hasFixedHeader:e,children:a.jsx(i,{children:l.map(r=>a.jsx(g,{children:r},`column-header-${r}`))})}),a.jsx(j,{children:[...d,...d].map(r=>a.jsx(i,{children:r.map((o,m)=>a.jsx(w,{columnWidth:S[l[m]],dataLabel:l[m],children:H(m,o)},`column-${l[m]}-${o}`))},r.toString()))})]})}),s=W.bind({});s.args={isFixed:!0,hasFixedHeader:!0};var p,u,x;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`({
+  isFixed
+}) => {
+  return <Table isFixed={isFixed}>
+      <TableHead>
+        <TableRow>
+          {columns.map((column: string) => <ColumnHeader key={\`column-header-\${column}\`}>
+              {column}
+            </ColumnHeader>)}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row: string[]) => <TableRow key={row.toString()}>
+            {row.map((content: string, i: number) => <DataCell columnWidth={columnSizes[columns[i]]} dataLabel={columns[i]} key={\`column-\${columns[i]}-\${content}\`}>
+                {renderContent(i, content)}
+              </DataCell>)}
+          </TableRow>)}
+      </TableBody>
+    </Table>;
+}`,...(x=(u=t.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};var b,T,h;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`({
+  isFixed,
+  hasFixedHeader
+}) => {
+  return <div style={{
+    height: '200px',
+    overflow: 'auto'
+  }}>
+      <Table isFixed={isFixed}>
+        <TableHead hasFixedHeader={hasFixedHeader}>
+          <TableRow>
+            {columns.map((column: string) => <ColumnHeader key={\`column-header-\${column}\`}>
+                {column}
+              </ColumnHeader>)}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {[...rows, ...rows].map((row: string[]) => <TableRow key={row.toString()}>
+              {row.map((content: string, i: number) => <DataCell columnWidth={columnSizes[columns[i]]} dataLabel={columns[i]} key={\`column-\${columns[i]}-\${content}\`}>
+                  {renderContent(i, content)}
+                </DataCell>)}
+            </TableRow>)}
+        </TableBody>
+      </Table>
+    </div>;
+}`,...(h=(T=s.parameters)==null?void 0:T.docs)==null?void 0:h.source}}};const le=["Default","StickyHeaderExample"];export{t as Default,s as StickyHeaderExample,le as __namedExportsOrder,oe as default};
