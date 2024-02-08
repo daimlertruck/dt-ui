9d756b21af7b585195f05c174e14165d9f2884ac
dt-ui <dt-ui@daimlertruck.com>
2024-02-08 14:45:15 +0000

    chore: update and publish Storybook

---
+++ PR-147/assets/Table.styled-OOPT3oaP.js
@@ -0,0 +1,127 @@
+import"./jsx-runtime-vNq4Oc-g.js";import"./index-5dULtSc_.js";import"./index-jmm5gWkb.js";import{T as n}from"./z-index-zwCKGdcf.js";import"./index-4g5l5LRQ.js";import{n as t}from"./emotion-styled.browser.esm-QTPHyGVR.js";const b=t.table`
+  ${({isFixed:o})=>`
+    table-layout: ${o?"fixed":"auto"};
+    width: 100%;
+    border-spacing: 0;
+  `}
+`,$=t.tbody`
+  ${({theme:o})=>`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: ${o.spacing["3xs"]};
+    
+    tr:hover {
+      background-color: ${o.palette.base.light};
+    }
+
+    @media (min-width: ${o.breakpoints.s}) {
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: ${o.breakpoints.m}) {
+      display: table-header-group;
+    }
+  `}
+`,c=t.thead`
+  ${({theme:o,hasFixedHeader:a})=>`
+    display: none;
+
+    tr {
+      background: transparent;
+    }
+
+    @media (min-width: ${o.breakpoints.m}) {
+      display: table-header-group;
+
+      ${a&&`
+          position: sticky;
+          top: 0;
+          background-color: ${o.palette.base.default};
+          z-index: ${n};
+          box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05)
+        `}
+    }
+`}
+`,g=t.tr`
+  ${({theme:o,selectableRow:a=!0})=>`
+    display: block;
+    padding: ${o.spacing.xs};
+    width: 100%;
+    background-color: ${o.palette.base.default};
+    border-radius: ${o.radius["4xs"]};
+    box-shadow: ${o.shadows.s};
+    cursor: ${a?"pointer":"auto"};
+    flex: 1;
+
+    @media (min-width: ${o.breakpoints.m}) {
+      display: table-row;
+      border-radius: ${o.radius.none};
+      box-shadow: ${o.shadows.none};
+      padding: ${o.spacing.none};
+    }
+
+  `}
+`,y=t.th`
+  ${({theme:o,textAlign:a})=>`
+    text-align: ${a};
+    color: ${o.palette.content.heading};
+    padding: 0 ${o.spacing["2xs"]} ${o.spacing["2xs"]};
+    ${o.fontStyles.button3};
+  `}
+`,f=t.td`
+  ${({theme:o,columnWidth:a,textAlign:d})=>`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: ${o.spacing["3xs"]};
+    padding: ${o.spacing["3xs"]} ${o.spacing["2xs"]};
+    word-break: break-word;
+    text-align: ${d};
+    color: ${o.palette.content.body};
+    ${o.fontStyles.body2};
+
+    &:not(:first-of-type){
+      &:before {
+        content: attr(data-label);
+        color: ${o.palette.content.heading};
+        word-break: keep-all;
+        ${o.fontStyles.button2};
+      }
+    }
+
+    &:first-of-type {
+      color: ${o.palette.content.heading};
+      border-bottom: 1px solid ${o.palette.border.light};
+      padding-top: 0;
+      ${o.fontStyles.body2Bold};
+    }
+
+    @media (min-width: ${o.breakpoints.m}) {
+      display: table-cell;      
+      border-top: 1px solid ${o.palette.border.light};
+      padding: ${o.spacing["2xs"]};
+      max-width: 0;
+      ${o.fontStyles.body2};
+      
+      &:first-of-type {
+        color: ${o.palette.content.body};
+        padding-top: ${o.spacing["2xs"]};
+        border-bottom: none;
+        ${o.fontStyles.body2};
+      }
+
+      &:before {
+        display: none;
+      }
+      
+      word-break: break-word;
+
+      ${!!a&&`
+        white-space: unset;
+        width: ${a};
+        max-width: ${a};
+      `}
+    }
+
+  `}
+`;export{y as C,g as R,b as T,c as a,$ as b,f as c};
---
+++ PR-147/assets/Tooltip.stories-eLknyMW6.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{n as h}from"./emotion-styled.browser.esm-QTPHyGVR.js";import"./Box.styled-zcGhU7kj.js";import"./Typography-D6FO4u36.js";import"./Spinner.styled-euKDJvzq.js";import"./Button.styled-bXS5oslU.js";import{I as u}from"./IconButton-q8eJfSTD.js";import{T as r,a as s,O as a}from"./Tooltip-UJg20KzB.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";import"./MultiSelectProvider-vrdgqO57.js";import"./emotion-react.browser.esm-OUs2k2GK.js";import"./IconButton.styled-B_RRHdai.js";import"./index-5dULtSc_.js";import"./index-jmm5gWkb.js";import"./index-XRl7aEt-.js";import"./util-p6oSlpjm.js";import"./index-5tMk2lpP.js";import"./Portal-EY94Zi_-.js";import"./z-index-zwCKGdcf.js";const g=e=>o.jsxs("svg",{fill:"none",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg",...e,children:[o.jsxs("g",{clipPath:"url(#clip0_287_3834)",children:[o.jsx("path",{d:"M8 9H16V19H8V9Z",fill:"currentColor",opacity:"0.3"}),o.jsx("path",{d:"M15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5ZM6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9Z",fill:"currentColor"})]}),o.jsx("defs",{children:o.jsx("clipPath",{id:"clip0_287_3834",children:o.jsx("rect",{fill:"white",height:"24",width:"24"})})})]}),x=h.div`
+  text-align: center;
+  margin-top: 100px;
+`,P={component:r,title:"Data Display/Tooltip",argTypes:{background:{options:s,control:{type:"radio"}},direction:{options:a,control:{type:"select"}},children:{table:{disable:!0}}},render:({children:e,hideDelay:c,background:p,direction:d,message:m})=>o.jsx(x,{children:o.jsxs(r,{hideDelay:c,children:[e,o.jsx(r.Content,{background:p,direction:d,children:m})]})})},t={args:{hideDelay:0,children:o.jsx(u,{color:"error",dataTestId:"delete-id-button-",isDisabled:!1,onClick:()=>console.log("log"),children:o.jsx(g,{})}),direction:a.Top,background:s.Full,message:"Delete"}};var i,n,l;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    hideDelay: 0,
+    children: <IconButton color='error' dataTestId='delete-id-button-' isDisabled={false} onClick={() => console.log('log')}>
+        <DeleteIcon />
+      </IconButton>,
+    direction: OTooltipDirection.Top,
+    background: OTooltipBackground.Full,
+    message: 'Delete'
+  }
+}`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const R=["Default"];export{t as Default,R as __namedExportsOrder,P as default};
