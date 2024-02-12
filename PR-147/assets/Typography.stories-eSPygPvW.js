d14bcd04be1c848c3cc302eb01531a3c9354e527
dt-ui <dt-ui@daimlertruck.com>
2024-02-12 17:22:45 +0000

    chore: delete PR preview

--- PR-147/assets/Typography.stories-eSPygPvW.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./jsx-runtime-vNq4Oc-g.js";import{T as p}from"./Typography-D6FO4u36.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";import"./MultiSelectProvider-vrdgqO57.js";const g={title:"Data Display/Typography",component:p},m=({children:a,...s})=>n.jsx(p,{...s,children:a}),r=m.bind({});r.args={children:"My Text",element:"h1",fontStyles:"h1",color:"black"};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
-  children,
-  ...props
-}) => {
-  return <Typography {...props}>{children}</Typography>;
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,g as default};
