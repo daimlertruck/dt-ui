d451505e4f6039b1dabd9e1f43da2b45b51a72a4
dt-ui <dt-ui@daimlertruck.com>
2024-02-14 12:00:47 +0000

    chore: update and publish Storybook

---
+++ PR-158/assets/Counter.stories-C6RhadM2.js
@@ -0,0 +1,5 @@
+import{j as a}from"./jsx-runtime-vNq4Oc-g.js";import{C as n}from"./Counter-LGyezUq9.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";const C={title:"Data Display/Counter",component:n},i=({...e})=>a.jsx(n,{...e,children:e.children}),r=i.bind({});r.args={children:"2",color:"informative",outlined:!1,isLarge:!1};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  ...props
+}) => {
+  return <Counter {...props}>{props.children}</Counter>;
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const x=["Default"];export{r as Default,x as __namedExportsOrder,C as default};
---
+++ PR-158/assets/DescriptionList.stories-wwXkQNdg.js
@@ -0,0 +1,18 @@
+import{j as r}from"./jsx-runtime-vNq4Oc-g.js";import"./MultiSelectProvider-vrdgqO57.js";import{L as p}from"./constants-libmexsO.js";import{D as i}from"./DescriptionList-DLLpITdb.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";const u={title:"Data Display/DescriptionList",component:i},c=({...n})=>r.jsxs(i,{...n,children:[r.jsx(i.Term,{children:"Term 1"}),r.jsx(i.Description,{children:"Some description 1"}),r.jsx(i.Term,{children:"Term 2"}),r.jsx(i.Description,{children:"Some description 2"}),r.jsx(i.Term,{children:"Term 3"}),r.jsx(i.Description,{children:"Some description 3"})]}),e=c.bind({});e.args={listStyleType:p.Disc};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`({
+  ...props
+}) => {
+  return <DescriptionList {...props}>
+      <DescriptionList.Term>Term 1</DescriptionList.Term>
+      <DescriptionList.Description>
+        Some description 1
+      </DescriptionList.Description>
+      <DescriptionList.Term>Term 2</DescriptionList.Term>
+      <DescriptionList.Description>
+        Some description 2
+      </DescriptionList.Description>
+      <DescriptionList.Term>Term 3</DescriptionList.Term>
+      <DescriptionList.Description>
+        Some description 3
+      </DescriptionList.Description>
+    </DescriptionList>;
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,u as default};
