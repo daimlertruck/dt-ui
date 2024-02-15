d603215ef2f9d7512c9bc07e8940927fa60f457d
dt-ui <dt-ui@daimlertruck.com>
2024-02-15 11:58:38 +0000

    chore: update and publish Storybook

---
+++ PR-162/assets/useMedia-S1QLeel4.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-4g5l5LRQ.js";const o=s=>{const[t,a]=n.useState(!1);return n.useEffect(()=>{const e=window.matchMedia(s);e.matches!==t&&a(e.matches);const c=()=>a(e.matches);return e.addEventListener("change",c),()=>e.removeEventListener("change",c)},[s,t]),t};export{o as u};
