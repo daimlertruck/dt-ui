29bc401255f9b46d7d2027b4fce39ac675ea2e31
dt-ui <dt-ui@daimlertruck.com>
2024-02-20 11:21:30 +0000

    chore: delete PR preview

--- PR-167/assets/Divider.styled-s1zDPfzJ.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./emotion-styled.browser.esm-QTPHyGVR.js";const t=e.hr`
-  border: 0px;
-  border-top: ${({theme:r})=>`1px ${r.palette.border.default} solid`};
-  margin-block: ${({theme:r})=>r.spacing.small};
-  width: 100%;
-`;export{t as D};
