5435525a6267c5787bc86a395269654ed36fc0ee
dt-ui <dt-ui@daimlertruck.com>
2024-02-21 19:08:25 +0000

    chore: update and publish Storybook

---
+++ PR-169/assets/Icon.styled-07V_RhS0.js
@@ -0,0 +1,27 @@
+import{n as e}from"./emotion-styled.browser.esm-QTPHyGVR.js";const a=e.i`
+  ${({fontSize:o,theme:t,variant:n,color:i})=>`
+    font-family: DTUI-icons-${t.icons};
+    font-weight: normal;
+    font-style: normal;
+    font-size: ${o};
+    display: inline-block;
+    line-height: 1;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    font-variation-settings: 'FILL' ${n==="filled"?1:0};
+    color: ${i};
+
+    /* Support for all WebKit browsers. */
+    -webkit-font-smoothing: antialiased;
+    /* Support for Safari and Chrome. */
+    text-rendering: optimizeLegibility;
+  
+    /* Support for Firefox. */
+    -moz-osx-font-smoothing: grayscale;
+  
+    /* Support for IE. */
+    font-feature-settings: 'liga';
+  `}
+`;export{a as I};
