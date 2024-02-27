5d05e3023c5d726f423f5934ec2dbc691adff638
dt-ui <dt-ui@daimlertruck.com>
2024-02-27 10:31:42 +0000

    chore: update and publish Storybook

---
+++ PR-177/assets/Form-5GciWGZG.js
@@ -0,0 +1,97 @@
+import{n as e}from"./emotion-styled.browser.esm-QTPHyGVR.js";import{j as l}from"./jsx-runtime-vNq4Oc-g.js";import{T as s,a as d}from"./Tooltip-7z1FKPw1.js";import{I as p}from"./Message--86pBn_H.js";import{T as c}from"./Typography-D6FO4u36.js";e.button`
+  ${({theme:t,disabled:o})=>`
+    background-color: transparent;
+    border: none;
+    cursor: ${o?"default":"pointer"};
+    display: flex;
+    align-items: center;
+    gap: 2;
+
+    svg:first-of-type {
+      margin-left: ${t.spacing.small};
+    }
+  `}
+`;const w=e.div`
+  display: block;
+  height: auto;
+  width: 100%;
+  position: relative;
+`,j=e.input`
+  ${({theme:t,hasError:o,disabled:i})=>`
+    ${t.fontStyles.pSmall}
+
+    background-color: ${i?t.palette.base.light:t.palette.base.default};
+    border: 1px solid ${t.palette.border.default};
+    border-radius: ${t.radius["6xs"]};
+
+    // Change border based on input status
+    border: ${o?`1px solid ${t.palette.error.default}`:`1px solid ${t.palette.border.default}`};
+
+    color: ${i?t.palette.content.disabled:t.palette.content.body};
+    min-width: 198px;
+    padding: 20px 15px 5px 15px;
+    transition: all 0.2s ease-in-out;
+    width: 100%;
+    height: 53px;
+  `}
+`,v=e.span`
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  width: 24px;
+  height: 24px;
+  cursor: not-allowed;
+  color: ${({theme:t})=>t.palette.error.default};
+`,T=e.div`
+  padding-left: 15px;
+`,F=e.span`
+  align-items: center;
+  background-color: ${({theme:t})=>t.palette.base.default};
+  cursor: pointer;
+  display: flex;
+  flex-flow: column nowrap;
+  height: 35px;
+  justify-content: space-around;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  width: 35px;
+
+  svg {
+    fill: ${({theme:t})=>t.palette.content.body};
+    transition: all 0.2s ease-in-out;
+  }
+
+  &:hover {
+    svg {
+      fill: ${({theme:t})=>t.palette.content.heading};
+    }
+  }
+`,u=e.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  row-gap: ${({theme:t})=>t.spacing.medium};
+`,x=e.div`
+  row-gap: ${({theme:t})=>t.spacing.xmedium};
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  color: ${({theme:t})=>t.palette.content.body};
+`,f=e.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 10px;
+  align-items: center;
+`,g=e.div`
+  ${({theme:t,disabled:o})=>`
+    display: flex;
+    column-gap: ${t.spacing.xmedium};
+    justify-content: stretch;
+    align-items: start;
+    ${o&&`
+        opacity: 0.5;
+        pointer-events: none;
+      `}
+  `}
+`,m=t=>t.preventDefault(),a=({children:t,dataTestId:o,style:i,onSubmit:n=m})=>{const r=o??"test-form";return l.jsx(u,{"data-testid":r,onSubmit:n,style:i,children:t})};a.Group=({children:t,isDisabled:o,title:i,tooltip:n})=>l.jsxs(x,{children:[i?l.jsxs(f,{children:[l.jsx(c,{color:"primary.default",fontStyles:"h6",style:{textTransform:"uppercase"},children:i}),n?l.jsxs(s,{children:[l.jsx(p,{height:"16",width:"16"}),l.jsx(s.Content,{direction:d.Right,style:{maxWidth:250},children:n})]}):null]}):null,l.jsx(g,{disabled:o,children:t})]});const I=a;export{I as F,v as I,F as S,w as T,j as a,T as b};
---
+++ PR-177/assets/Pagination.stories-_XCvHr6t.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{r as l}from"./index-4g5l5LRQ.js";import{P as r,a as p}from"./Pagination-p6S72Nd3.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-g_mmjWZx.js";import"./emotion-react.browser.esm-OUs2k2GK.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";import"./extends-E30VzY7q.js";import"./index-jmm5gWkb.js";import"./index-XRl7aEt-.js";import"./util-p6oSlpjm.js";import"./index-5tMk2lpP.js";import"./debounce-ctlr7l9d.js";import"./Typography-D6FO4u36.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./MultiSelectProvider-vrdgqO57.js";const m=()=>{const[e,n]=l.useState(1),t=a=>{const d=["Enter","Backspace","Tab"];!/Digit\d/.test(a.code)&&!d.includes(a.code)&&a.preventDefault()},s=l.useCallback(a=>{n(a)},[]);return{currentPage:e,handleChange:s,handleUserInput:t}},K={title:"Data Display/Pagination",component:r},P=({totalPages:e})=>{const{currentPage:n,handleChange:t,handleUserInput:s}=m();return o.jsxs(r,{children:[o.jsx(r.PreviousItem,{disabled:n<=1||n>e,onClick:()=>t(n-1)}),o.jsx(p,{onChange:t,onHandleKeyDown:s,totalPages:e,value:n}),o.jsx(r.Content,{onClick:()=>t(e),totalPages:e}),o.jsx(r.NextItem,{disabled:n>=e||n<1,onClick:()=>t(n+1)})]})},i=P.bind({});i.args={totalPages:10};var c,g,u;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`({
+  totalPages
+}) => {
+  const {
+    currentPage,
+    handleChange,
+    handleUserInput
+  } = usePagination();
+  return <Pagination>
+      <Pagination.PreviousItem disabled={currentPage <= 1 || currentPage > totalPages} onClick={() => handleChange(currentPage - 1)} />
+      <PaginationInput onChange={handleChange} onHandleKeyDown={handleUserInput} totalPages={totalPages} value={currentPage} />
+      <Pagination.Content onClick={() => handleChange(totalPages)} totalPages={totalPages} />
+      <Pagination.NextItem disabled={currentPage >= totalPages || currentPage < 1} onClick={() => handleChange(currentPage + 1)} />
+    </Pagination>;
+}`,...(u=(g=i.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};const N=["Default"];export{i as Default,N as __namedExportsOrder,K as default};
