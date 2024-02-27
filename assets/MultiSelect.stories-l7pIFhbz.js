f1e56402abce3c672c8a3003fff8ccbaae4a6d2f
dt-ui <dt-ui@daimlertruck.com>
2024-02-27 11:37:39 +0000

    chore: update and publish Storybook

---
+++ assets/MultiSelect.stories-l7pIFhbz.js
@@ -0,0 +1,14 @@
+import{j as l}from"./jsx-runtime-vNq4Oc-g.js";import{M as t}from"./MultiSelect-EO0nnqG1.js";import"./MultiSelectProvider-vrdgqO57.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-lOH_pZ5X.js";import"./Box.styled-zcGhU7kj.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";import"./Close-fCiS4iy8.js";import"./Tag-t4dOy4wM.js";import"./DropdownMenu-mGAn_RpM.js";import"./index-g_mmjWZx.js";import"./emotion-react.browser.esm-OUs2k2GK.js";import"./index-jmm5gWkb.js";import"./index-XRl7aEt-.js";import"./util-p6oSlpjm.js";import"./index-5tMk2lpP.js";import"./z-index-tR9610fR.js";import"./Typography-D6FO4u36.js";import"./useClickOutside-5i9YsATO.js";const _={title:"Data Display/MultiSelect",component:t},r=[{text:"Option 1",value:"1"},{text:"Option 2",value:"2",isRemovable:!0},{text:"Option 3",value:"3",isRemovable:!0},{text:"Option 4",value:"4",isRemovable:!0}],e=({options:n,...s})=>l.jsx(t,{defaultValue:[r[0],r[1]],children:l.jsx(t.Container,{children:l.jsx(t.Select,{label:"Label",placeholder:"Select the Items",...s,children:n.map(o=>l.jsx(t.Option,{option:o,children:o.text??o.value},o.value))})})});e.args={options:r,hasError:!1,isDisabled:!1};var i,p,a;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`({
+  options,
+  ...props
+}: MultiSelectStoryProps) => {
+  return <MultiSelect defaultValue={[OPTIONS[0], OPTIONS[1]]}>
+      <MultiSelect.Container>
+        <MultiSelect.Select label='Label' placeholder='Select the Items' {...props}>
+          {options.map(option => <MultiSelect.Option key={option.value} option={option}>
+              {option.text ?? option.value}
+            </MultiSelect.Option>)}
+        </MultiSelect.Select>
+      </MultiSelect.Container>
+    </MultiSelect>;
+}`,...(a=(p=e.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,_ as default};
