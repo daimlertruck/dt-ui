f72e01c518ab7fed1c0732c3375c8a4ff585fbb9
dt-ui <dt-ui@daimlertruck.com>
2024-02-27 18:38:01 +0000

    chore: update and publish Storybook

---
+++ PR-179/assets/Divider-b2fMfDtF.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{n as e}from"./emotion-styled.browser.esm-QTPHyGVR.js";const o=e.hr`
+  border: 0px;
+  border-top: ${({theme:r})=>`1px ${r.palette.border.default} solid`};
+  margin-block: ${({theme:r})=>r.spacing.small};
+  width: 100%;
+`,d=({style:r})=>t.jsx(o,{style:r});export{d as D};
