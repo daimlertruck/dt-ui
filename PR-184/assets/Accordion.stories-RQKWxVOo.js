ad29dec715e9c3b359e29f55d5c68016a501649b
dt-ui <dt-ui@daimlertruck.com>
2024-02-28 17:05:37 +0000

    chore: delete PR preview

--- PR-184/assets/Accordion.stories-RQKWxVOo.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{A as c,a as t}from"./Accordion-h2BS8qQ8.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";import"./Divider-b2fMfDtF.js";const f={title:"Data Display/Accordion",component:c,argTypes:{backgroundColor:{mapping:t,options:Object.values(t).filter(r=>typeof r=="string"),control:{type:"inline-radio"}}}},e=({...r})=>{const i=o.jsx(o.Fragment,{children:"Header content here."});return o.jsx(c,{...r,headerContent:i,children:"Body content here."})};e.args={isOpen:!1,backgroundColor:t.White,isDisabled:!1,headerContent:o.jsx(o.Fragment,{children:"Header content here."})};var n,a,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`({
-  ...props
-}: AccordionBaseProps) => {
-  const headerCustomContent = <>Header content here.</>;
-  return <Accordion {...props} headerContent={headerCustomContent}>
-      Body content here.
-    </Accordion>;
-}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const A=["Default"];export{e as Default,A as __namedExportsOrder,f as default};
