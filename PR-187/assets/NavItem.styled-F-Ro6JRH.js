6e391c7319c48715fb349c2dc41ed0bff1c049e8
dt-ui <dt-ui@daimlertruck.com>
2024-02-29 11:10:59 +0000

    chore: update and publish Storybook

---
+++ PR-187/assets/NavItem.styled-F-Ro6JRH.js
@@ -0,0 +1,18 @@
+import{n as a}from"./emotion-styled.browser.esm-QTPHyGVR.js";const e=a.li`
+  ${({theme:l,matchRoute:t})=>`
+    ${l.fontStyles.pSmall};
+    display: inline-block;
+    list-style: none;
+    padding-left: 0px;
+
+    a {
+      color: ${l.palette.primary.contrast};
+      cursor: pointer;
+    }
+
+    a:link,
+    a:visited {
+      color: ${t?l.palette.primary.default:""};
+    }
+  `}
+`;export{e as N};
