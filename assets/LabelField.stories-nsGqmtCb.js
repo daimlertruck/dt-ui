6524c6e64fb4b09c605f53cf55dba4bb475c2a91
dt-ui <dt-ui@daimlertruck.com>
2024-02-29 11:33:28 +0000

    chore: update and publish Storybook

---
+++ assets/LabelField.stories-nsGqmtCb.js
@@ -0,0 +1,7 @@
+import{j as l}from"./jsx-runtime-vNq4Oc-g.js";import{L as a}from"./LabelField-da40idZh.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./emotion-styled.browser.esm-QTPHyGVR.js";import"./extends-E30VzY7q.js";import"./emotion-element-c39617d8.browser.esm-4Kx8mvQ5.js";const b={title:"Data Display/LabelField",component:a,argTypes:{},render:({children:i,...o})=>l.jsx(a,{...o,children:i})},e={args:{children:"My Label",isActive:!1,isDisabled:!1}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'My Label',
+    isActive: false,
+    isDisabled: false
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const D=["Default"];export{e as Default,D as __namedExportsOrder,b as default};
