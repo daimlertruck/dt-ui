3a3214f4c769426f128ec7e7fe5d612c6f03c271
dt-ui <dt-ui@daimlertruck.com>
2024-03-01 15:33:14 +0000

    chore: update and publish Storybook

---
+++ assets/List.stories-DY-kIHuH.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{L as e}from"./List-Dp0yI3fY.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Typography-BEz-Klo6.js";import"./index-qwY8W0Yo.js";import"./emotion-react.browser.esm-DebaLaFy.js";import"./emotion-element-43c6fea0.browser.esm-DXL8qHXx.js";import"./extends-ChRHEBve.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./emotion-styled.browser.esm-CRzct0lr.js";import"./DropdownProvider-D86Tot7W.js";import"./MultiSelectProvider-Cu0vsbwR.js";import"./constants-BPg6vsiE.js";const y={title:"Data Display/List",component:e},p=m=>r.jsxs(e,{...m,children:[r.jsx("li",{children:"Item 1"}),r.jsx("li",{children:"Item 2"})]}),t=p.bind({});var i,o,s;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`props => {
+  return <List {...props}>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </List>;
+}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const O=["Default"];export{t as Default,O as __namedExportsOrder,y as default};
---
+++ assets/Tooltip.stories-BWegrEiu.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{n as h}from"./emotion-styled.browser.esm-CRzct0lr.js";import"./Box.styled-OXY0hm-i.js";import"./Typography-BEz-Klo6.js";import"./Spinner.styled-DLX8sObT.js";import"./Button.styled-BaZIR7EZ.js";import{I as u}from"./IconButton-Cy6QCa2K.js";import{T as r,a as s,O as a}from"./Tooltip-Cvc7Dz4-.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-ChRHEBve.js";import"./emotion-element-43c6fea0.browser.esm-DXL8qHXx.js";import"./index-qwY8W0Yo.js";import"./emotion-react.browser.esm-DebaLaFy.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./IconButton.styled-ppP7-P7p.js";import"./Portal-U_0UEmB3.js";import"./z-index-Bx-k-k_v.js";const g=e=>o.jsxs("svg",{fill:"none",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg",...e,children:[o.jsxs("g",{clipPath:"url(#clip0_287_3834)",children:[o.jsx("path",{d:"M8 9H16V19H8V9Z",fill:"currentColor",opacity:"0.3"}),o.jsx("path",{d:"M15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5ZM6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9Z",fill:"currentColor"})]}),o.jsx("defs",{children:o.jsx("clipPath",{id:"clip0_287_3834",children:o.jsx("rect",{fill:"white",height:"24",width:"24"})})})]}),x=h.div`
+  text-align: center;
+  margin-top: 100px;
+`,L={component:r,title:"Data Display/Tooltip",argTypes:{background:{options:s,control:{type:"radio"}},direction:{options:a,control:{type:"select"}},children:{table:{disable:!0}}},render:({children:e,hideDelay:c,background:p,direction:d,message:m})=>o.jsx(x,{children:o.jsxs(r,{hideDelay:c,children:[e,o.jsx(r.Content,{background:p,direction:d,children:m})]})})},t={args:{hideDelay:0,children:o.jsx(u,{color:"error",dataTestId:"delete-id-button-",isDisabled:!1,onClick:()=>console.log("log"),children:o.jsx(g,{})}),direction:a.Top,background:s.Full,message:"Delete"}};var i,n,l;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    hideDelay: 0,
+    children: <IconButton color='error' dataTestId='delete-id-button-' isDisabled={false} onClick={() => console.log('log')}>
+        <DeleteIcon />
+      </IconButton>,
+    direction: OTooltipDirection.Top,
+    background: OTooltipBackground.Full,
+    message: 'Delete'
+  }
+}`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const P=["Default"];export{t as Default,P as __namedExportsOrder,L as default};
