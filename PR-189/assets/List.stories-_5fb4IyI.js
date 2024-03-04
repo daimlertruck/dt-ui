e500bc915e848329dc8edc5fcce863549b32eda2
dt-ui <dt-ui@daimlertruck.com>
2024-03-04 14:07:32 +0000

    chore: delete PR preview

--- PR-189/assets/List.stories-_5fb4IyI.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./jsx-runtime-CKrituN3.js";import{L as e}from"./List-cBy4LZB2.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Typography-DQWzsPBO.js";import"./index-Doi4ik1y.js";import"./emotion-react.browser.esm-DebaLaFy.js";import"./emotion-element-43c6fea0.browser.esm-DXL8qHXx.js";import"./extends-ChRHEBve.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./emotion-styled.browser.esm-CRzct0lr.js";import"./DropdownProvider-D86Tot7W.js";import"./MultiSelectProvider-Cu0vsbwR.js";import"./constants-BPg6vsiE.js";const y={title:"Data Display/List",component:e},p=m=>r.jsxs(e,{...m,children:[r.jsx("li",{children:"Item 1"}),r.jsx("li",{children:"Item 2"})]}),t=p.bind({});var i,o,s;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`props => {
-  return <List {...props}>
-      <li>Item 1</li>
-      <li>Item 2</li>
-    </List>;
-}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const O=["Default"];export{t as Default,O as __namedExportsOrder,y as default};
--- PR-189/assets/Spinner.stories-BdlPONoE.js
+++
@@ -1,7 +0,0 @@
-import{S as i}from"./Spinner-wlfpEqi9.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtDBdDin.js";import"./Box.styled-OXY0hm-i.js";import"./emotion-styled.browser.esm-CRzct0lr.js";import"./extends-ChRHEBve.js";import"./emotion-element-43c6fea0.browser.esm-DXL8qHXx.js";import"./Typography-DQWzsPBO.js";import"./index-Doi4ik1y.js";import"./emotion-react.browser.esm-DebaLaFy.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./Spinner.styled-DLX8sObT.js";const z={title:"Data Display/Spinner",component:i,parameters:{backgrounds:{values:[{name:"black",value:"#000"}]}},argTypes:{size:{options:["small","medium","large","extra-large"],control:{type:"select"}},colorScheme:{options:["positive","negative"],control:{type:"radio"}}}},e={args:{size:"medium",colorScheme:"positive",loadingText:""}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 'medium',
-    colorScheme: 'positive',
-    loadingText: ''
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,z as default};
