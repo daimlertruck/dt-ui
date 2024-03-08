5ca03058aa283b02bd9f1be1b75d622eb21e0a57
dt-ui <dt-ui@daimlertruck.com>
2024-03-08 17:00:29 +0000

    chore: update and publish Storybook

---
+++ PR-194/assets/Button.stories-V1H2Hldc.js
@@ -0,0 +1,10 @@
+import{B as m}from"./Button-C37JXO44.js";import"./Button.styled-EVogWNBH.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Spinner-DOiPSzr0.js";import"./index-gq_RjDZX.js";import"./Box.styled-B8MVCI1k.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";import"./Typography-IFB7dyWN.js";import"./index-BpxOyJIn.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./Spinner.styled-Bj9pCZ86.js";const z={title:"Data Display/Button",component:m},o={args:{children:"Button",onClick:()=>null,isLoading:!1,variant:"solid",color:"primary",size:"medium"}};var r,t,i;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    onClick: () => null,
+    isLoading: false,
+    variant: 'solid',
+    color: 'primary',
+    size: 'medium'
+  }
+}`,...(i=(t=o.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const C=["Default"];export{o as Default,C as __namedExportsOrder,z as default};
