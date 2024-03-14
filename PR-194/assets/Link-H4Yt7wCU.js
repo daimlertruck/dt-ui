7a2214ebaccacdaee3e4af3d5b1decb6978e62a3
dt-ui <dt-ui@daimlertruck.com>
2024-03-14 18:27:20 +0000

    chore: update and publish Storybook

---
+++ PR-194/assets/Link-H4Yt7wCU.js
@@ -0,0 +1,24 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{r as u}from"./index-CBqU2yxZ.js";import"./Box.styled-B8MVCI1k.js";import"./Typography-DeSH24ip.js";import"./Spinner.styled-BohM9upl.js";import{B as $}from"./Button.styled-CrKUpX37.js";import{n as f}from"./emotion-styled.browser.esm-DhD7UCCJ.js";var r=(t=>(t.Small="small",t.Medium="medium",t.Large="large",t))(r||{});const y=f.a`
+  ${({theme:t,disabled:a,isUnderline:l,textSize:o})=>`
+    color: ${t.palette.primary.default};
+    text-decoration: ${l?"underline":"none"};
+
+    ${a&&`
+        opacity: 0.4;
+        pointer-events: none;
+    
+      `};
+
+    ${o==r.Large&&`
+        ${t.fontStyles.p}
+      `};
+
+    ${o==r.Medium&&`
+        ${t.fontStyles.pSmall}
+      `};
+
+    ${o==r.Small&&`
+        ${t.fontStyles.pXXSmall}
+      `};
+  `}
+`,c=$.withComponent("a"),v=u.forwardRef(({isDisabled:t,children:a,isUnderline:l=!0,textSize:o=r.Medium,dataTestId:i,onClick:p,style:m,type:d="text",...s},e)=>n.jsx(n.Fragment,{children:d==="button"?n.jsx(c,{"data-testid":i??"link-button",ref:e,size:o,style:m,variant:"solid",...s,color:"primary",children:a}):n.jsx(y,{"data-testid":i??"link",disabled:t,isUnderline:l,onClick:p,ref:e,style:m,textSize:o,...s,children:a})}));export{v as L,r as a};
