fd725e4e24d0a75bd59ce2d6ca08e4519edd3447
dt-ui <dt-ui@daimlertruck.com>
2024-03-14 15:31:19 +0000

    chore: delete PR preview

--- PR-196/assets/preview-B3naNHPa.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-D_-RIGe4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DuxsCEJM.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DuxsCEJM.js","./iframe-D_-RIGe4.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DE3ZboNN.js","./chunk-EIRT5I3Z-CbnHSNsj.js","./extends-DJVMZnyl.js","./index-Bw8VTzHM.js","./index-qciFhAsL.js","./index-BAMY2Nnw.js","./util-BQ0cylCP.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
