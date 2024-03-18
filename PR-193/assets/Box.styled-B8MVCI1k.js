7189849cc4b00f413957f00a0e59bc011b42a206
dt-ui <dt-ui@daimlertruck.com>
2024-03-18 09:03:37 +0000

    chore: cleanup unused folders

--- PR-193/assets/Box.styled-B8MVCI1k.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./emotion-styled.browser.esm-DhD7UCCJ.js";const i=e.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;export{i as B};
--- PR-193/assets/IconButton.stories-vXE7zvRN.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{I as i}from"./Icon-BD8thEoO.js";import{I as p}from"./IconButton-BS2RgPV3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Icon.styled-CQdnWCSi.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";const g={title:"Data Display/IconButton",component:p,argTypes:{color:{mapping:["error","primary"],options:Object.values(["error","primary"]).filter(r=>typeof r=="string"),control:{type:"select"}}}},c=({...r})=>t.jsx(p,{...r,children:t.jsx(i,{code:"edit",color:"inherit"})}),o=c.bind({});o.args={color:"primary",onClick:()=>null};var e,n,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`({
-  ...props
-}) => {
-  return <IconButton {...props}>
-      <Icon code='edit' color='inherit' />
-    </IconButton>;
-}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const j=["Default"];export{o as Default,j as __namedExportsOrder,g as default};
