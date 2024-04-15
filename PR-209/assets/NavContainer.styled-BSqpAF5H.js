ebfab3210291af3e7025160a4ec2c2eb73d004b5
dt-ui <dt-ui@daimlertruck.com>
2024-04-15 09:02:28 +0000

    chore: cleanup unused folders

--- PR-209/assets/NavContainer.styled-BSqpAF5H.js
+++
@@ -1,8 +0,0 @@
-import{n}from"./emotion-styled.browser.esm-DhD7UCCJ.js";const i=n.div`
-  margin: ${({theme:e})=>e.spacing.none};
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: row-reverse;
-  min-width: 80%;
-`;export{i as N};
