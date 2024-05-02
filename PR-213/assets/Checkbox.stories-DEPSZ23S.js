088e71f1c1850f88137d13d6ba753fa5c54e2df4
dt-ui <dt-ui@daimlertruck.com>
2024-05-02 15:13:38 +0000

    chore: update and publish Storybook

---
+++ PR-213/assets/Checkbox.stories-DEPSZ23S.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{I as u}from"./Icon-CHFlL13I.js";import{C as f,a as b,b as k,c as j}from"./Checkbox.styled-sKHAuIgX.js";import{u as g}from"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";const y=({onChange:s,dataTestId:m,children:c,isChecked:t=!1,isDisabled:o=!1,style:d})=>{const a=m??"checkbox-id",i=g(),x=h=>{o||s==null||s(h)};return e.jsxs(f,{isChecked:t,isDisabled:o,style:d,children:[e.jsxs(b,{children:[e.jsx(k,{checked:t,"data-testid":a,disabled:o,id:a,onChange:x,type:"checkbox"}),t?e.jsx(u,{code:"check",color:i.palette.content.contrast}):null]}),c?e.jsx(j,{htmlFor:a,isChecked:t,isDisabled:o,children:c}):null]})},_={title:"Data Display/Checkbox",component:y,argTypes:{}},r={args:{isDisabled:!1,isChecked:!1,children:"Label",onChange:()=>null}};var n,l,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isDisabled: false,
+    isChecked: false,
+    children: 'Label',
+    onChange: () => null
+  }
+}`,...(p=(l=r.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const B=["Default"];export{r as Default,B as __namedExportsOrder,_ as default};
