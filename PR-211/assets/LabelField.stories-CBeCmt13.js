768a4d7bfe4ffaf47e7ca12dad808ee9fcb11141
dt-ui <dt-ui@daimlertruck.com>
2024-05-03 12:50:02 +0000

    chore: delete PR preview

--- PR-211/assets/LabelField.stories-CBeCmt13.js
+++
@@ -1,8 +0,0 @@
-import{j as l}from"./jsx-runtime-CKrituN3.js";import{L as s}from"./LabelField-yOfbUvkj.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";const b={title:"Data Display/LabelField",component:s,argTypes:{},render:({children:t,...o})=>l.jsx(s,{...o,children:t})},e={args:{children:"My Label",isActive:!1,isDisabled:!1,isRequired:!1}};var a,r,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    children: 'My Label',
-    isActive: false,
-    isDisabled: false,
-    isRequired: false
-  }
-}`,...(i=(r=e.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const D=["Default"];export{e as Default,D as __namedExportsOrder,b as default};
--- PR-211/assets/LabelField.stories-CS2yeioW.js
+++
@@ -1,8 +0,0 @@
-import{j as l}from"./jsx-runtime-CKrituN3.js";import{L as r}from"./LabelField-B64ZKOub.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";const b={title:"Data Display/LabelField",component:r,argTypes:{},render:({children:i,...o})=>l.jsx(r,{...o,children:i})},e={args:{children:"My Label",isActive:!1,isDisabled:!1,required:!1}};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    children: 'My Label',
-    isActive: false,
-    isDisabled: false,
-    required: false
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const D=["Default"];export{e as Default,D as __namedExportsOrder,b as default};
