c009ee1f089f762894b39a1ce2df2b1ea934cb13
Masoud Soleymani <msoleym@tbdir.net>
2024-05-23 11:37:02 +0100

    refactor: drop Header component
    Header component is specific to TruckAPI and is not supposed
    to live on Dynamo.

--- packages/dt-ui-react/index.ts
+++ packages/dt-ui-react/index.ts
@@ -6,7 +6,6 @@ export * from './core';
 // Components
 export * from './components/buttons';
 export * from './components/dropdown';
-export * from './components/header';
 export * from './components/card';
 export * from './components/form';
 export * from './components/link';
