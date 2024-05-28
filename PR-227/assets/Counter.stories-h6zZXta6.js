209eb92ef66c4505f003e25feeb66377a8f65d9c
dt-ui <dt-ui@daimlertruck.com>
2024-05-28 08:02:52 +0000

    chore: update and publish Storybook

---
+++ PR-227/assets/Counter.stories-h6zZXta6.js
@@ -0,0 +1,5 @@
+import{j as a}from"./jsx-runtime-CKrituN3.js";import{C as n}from"./Counter-BH71OkUS.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";const C={title:"Data Display/Counter",component:n},i=({...e})=>a.jsx(n,{...e,children:e.children}),r=i.bind({});r.args={children:"2",color:"informative",outlined:!1,isLarge:!1};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  ...props
+}) => {
+  return <Counter {...props}>{props.children}</Counter>;
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const x=["Default"];export{r as Default,x as __namedExportsOrder,C as default};
