141e3de6f6d4a6cc5eded1a766b8ef6c7c15537b
dt-ui <dt-ui@daimlertruck.com>
2024-05-29 17:19:10 +0000

    chore: delete PR preview

--- PR-226/assets/preview-CttvfBmp.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Bzchyp7Q.js","./iframe-BGmkyyfr.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-CJ2L83dN.js","./client-BvKKsruy.js","./index-BtM5VmRH.js","./index-D1Yj_IB1.js","./index-BbP3371Q.js","./chunk-EIRT5I3Z-CbnHSNsj.js","./extends-DJVMZnyl.js","./index-Bw8VTzHM.js","./index-Banbat9M.js","./index-BAMY2Nnw.js","./index-Bl7H5CcN.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-BGmkyyfr.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Bzchyp7Q.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]),import.meta.url);return new r}}};export{a as parameters};
