562a571b09069958366a853f5da3fee16491b55b
dt-ui <dt-ui@daimlertruck.com>
2024-07-08 21:35:33 +0000

    chore: update and publish Storybook

---
+++ PR-244/assets/Button.stories-BSpDMBF3.js
@@ -0,0 +1,46 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{B as p}from"./Button-D8JWryI2.js";import"./Button.styled-CrKUpX37.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Spinner-BRPytc7Z.js";import"./index-gq_RjDZX.js";import"./Box.styled-B8MVCI1k.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";import"./Typography-gBi7_xh9.js";import"./index-D2m8a1IB.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./Spinner.styled--jOA-jAc.js";const L={title:"Data Display/Button",component:p},o={args:{children:"Button",onClick:()=>null,isLoading:!1,variant:"solid",color:"primary",size:"medium",isDisabled:!1},argTypes:{variant:{options:["solid","outlined","text"],control:{type:"select"}},color:{options:["primary","secondary","error","contrast"],control:{type:"select"}},size:{options:["medium","large","small"],control:{type:"select"}},isDisabled:{control:"boolean"}}},r={...o,render:({...c})=>n.jsx("div",{style:{width:"200px",height:"100px",backgroundColor:"#000000",padding:"20px"},children:n.jsx(p,{...c})})};var t,e,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    onClick: () => null,
+    isLoading: false,
+    variant: 'solid',
+    color: 'primary',
+    size: 'medium',
+    isDisabled: false
+  },
+  argTypes: {
+    variant: {
+      options: ['solid', 'outlined', 'text'],
+      control: {
+        type: 'select'
+      }
+    },
+    color: {
+      options: ['primary', 'secondary', 'error', 'contrast'],
+      control: {
+        type: 'select'
+      }
+    },
+    size: {
+      options: ['medium', 'large', 'small'],
+      control: {
+        type: 'select'
+      }
+    },
+    isDisabled: {
+      control: 'boolean'
+    }
+  }
+}`,...(i=(e=o.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};var s,a,l;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...Default,
+  render: ({
+    ...props
+  }) => <div style={{
+    width: '200px',
+    height: '100px',
+    backgroundColor: '#000000',
+    padding: '20px'
+  }}>
+      <Button {...props} />
+    </div>
+}`,...(l=(a=r.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const S=["Default","buttonWithDarkBackground"];export{o as Default,S as __namedExportsOrder,r as buttonWithDarkBackground,L as default};
---
+++ PR-244/assets/Checkbox.stories-B4pUKcWZ.js
@@ -0,0 +1,8 @@
+import{C as o}from"./Checkbox-B6dJBJ-3.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Icon-CHFlL13I.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";const f={title:"Data Display/Checkbox",component:o,argTypes:{}},e={args:{isDisabled:!1,isChecked:!1,children:"Label",onChange:()=>null}};var a,r,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    isDisabled: false,
+    isChecked: false,
+    children: 'Label',
+    onChange: () => null
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,f as default};
