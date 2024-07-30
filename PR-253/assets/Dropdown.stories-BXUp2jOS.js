b4fb6c408b8f389c7f3022960b89d16b3093eaa3
dt-ui <dt-ui@daimlertruck.com>
2024-07-30 14:33:48 +0000

    chore: delete PR preview

--- PR-253/assets/Dropdown.stories-BXUp2jOS.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-CKrituN3.js";import{D as r}from"./Dropdown-BNvqEoC8.js";import"./Typography-AgncjmvB.js";import"./index-CBqU2yxZ.js";import"./Icon-CHFlL13I.js";import"./index-DWb54gmw.js";import"./index-BtM5VmRH.js";import"./emotion-styled.browser.esm-DhD7UCCJ.js";import"./extends-DJVMZnyl.js";import"./emotion-element-43c6fea0.browser.esm-CsO0Y6lU.js";import"./_commonjsHelpers-BosuxZz1.js";import"./z-index-Ds5QkyqS.js";import"./useClickOutside-D7JrBqcS.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";const d=[{text:"Option 1",value:"1"},{text:"Option 2",value:"2"},{text:"Option 3 with a large text message label",value:"3"},{text:"Option 4",value:"4"}],C={title:"Data Display/Dropdown",component:r,argTypes:{},render:({options:p,defaultValue:n,message:o,...m})=>e.jsx("div",{style:{height:"300px",padding:"10px",backgroundColor:"#EBECED"},children:e.jsx(r,{defaultValue:n,children:e.jsxs(r.Container,{children:[e.jsx(r.Select,{label:"Label",...m,children:p.map(t=>e.jsx(r.Option,{option:t,children:t.text??t.value},t.value))}),o?e.jsx(r.Detail,{children:o}):null]})})})},a={args:{options:d,isDisabled:!1,hasBorder:!0,hasError:!1,message:""}};var s,i,l;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    options: OPTIONS,
-    isDisabled: false,
-    hasBorder: true,
-    hasError: false,
-    message: ''
-  }
-}`,...(l=(i=a.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const T=["Default"];export{a as Default,T as __namedExportsOrder,C as default};
