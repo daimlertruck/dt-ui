5b3d34831e8ea51902d6521eea8627febc278850
dt-ui <dt-ui@daimlertruck.com>
2024-08-27 08:08:42 +0000

    chore: update and publish Storybook

---
+++ PR-267/assets/TextArea.stories-BG-FWW2-.js
@@ -0,0 +1,7 @@
+import{j as s}from"./jsx-runtime-CKrituN3.js";import{T as r}from"./TextArea-DvS5YDmq.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./LabelField-Cy5ugnCm.js";import"./Typography-DvHa2txy.js";import"./index-CJ73Y9Wi.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./TextArea.styled-BgdXJPw-.js";const b={title:"Data Display/TextArea",component:r,argTypes:{},render:({...m})=>s.jsx(r,{...m})},e={args:{label:"Awesome TextArea",maxLength:120,value:""}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Awesome TextArea',
+    maxLength: 120,
+    value: ''
+  }
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,b as default};
