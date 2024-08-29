e79b8aaccead6aa80fc2f5a611274b04c564cc7d
dt-ui <dt-ui@daimlertruck.com>
2024-08-29 15:30:49 +0000

    chore: update and publish Storybook

---
+++ PR-271/assets/Checkbox.stories-CJJErkeu.js
@@ -0,0 +1,8 @@
+import{C as o}from"./Checkbox-xG7MOetM.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Icon-DOElHRRq.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";const d={title:"Data Display/Checkbox",component:o,argTypes:{}},e={args:{isDisabled:!1,isChecked:!1,children:"Label",onChange:()=>null}};var a,r,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    isDisabled: false,
+    isChecked: false,
+    children: 'Label',
+    onChange: () => null
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,d as default};
