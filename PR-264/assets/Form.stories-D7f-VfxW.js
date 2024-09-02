7a6b5aef7341633f8537bc145fbeb22db2dfb870
dt-ui <dt-ui@daimlertruck.com>
2024-09-02 09:03:21 +0000

    chore: cleanup unused folders

--- PR-264/assets/Form.stories-D7f-VfxW.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-CKrituN3.js";import{T as e}from"./TextField-DZnW7J9Q.js";import"./TextField.styled-D7RLo66B.js";import{F as t}from"./Form-CYX7W-hq.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DMRWov4L.js";import"./Box.styled-TgnH4Hzf.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";import"./LabelField-Cy5ugnCm.js";import"./Typography-DvHa2txy.js";import"./index-CJ73Y9Wi.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./Icon-DOElHRRq.js";import"./Tooltip-B_S9_H1I.js";import"./Portal-U_0UEmB3.js";import"./z-index-Ds5QkyqS.js";const v={title:"Data Display/Form",component:t,render:({title:l,tooltip:n,isDisabled:i,isRequired:s})=>r.jsxs(t,{children:[r.jsx(t.Group,{isDisabled:i,isRequired:s,title:l,tooltip:n,children:r.jsx(e,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:i,isRequired:s,children:[r.jsx(e,{label:"label 2"}),r.jsx(e,{label:"label 3"})]})]})},o={args:{title:"",tooltip:"",isDisabled:!1,isRequired:!1}};var a,m,p;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    title: '',
-    tooltip: '',
-    isDisabled: false,
-    isRequired: false
-  }
-}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const w=["Default"];export{o as Default,w as __namedExportsOrder,v as default};
