404fe6707717c0e65a219bdab7168f968dd94e49
Pereira <joel.pereira@daimler.com>
2024-12-17 12:04:47 +0000

    feat: add helper and error state

--- packages/dt-ui-react/components/form/text-area/TextArea.styled.ts
+++ packages/dt-ui-react/components/form/text-area/TextArea.styled.ts
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled';
 
+export interface TextAreaStyledProps {
+  hasError: boolean;
+}
+
 export const TextAreaWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -8,12 +12,24 @@ export const TextAreaWrapper = styled.div`
   width: 100%;
 `;
 
-export const TextAreaStyled = styled.textarea`
-  ${({ theme }) => `
+export const InputFieldIconStyled = styled.span`
+  position: absolute;
+  right: 7px;
+  top: 14px;
+  width: 24px;
+  height: 24px;
+  color: ${({ theme }) => theme.palette.alpha.error};
+`;
+
+export const TextAreaStyled = styled.textarea<TextAreaStyledProps>`
+  ${({ theme, hasError }) => `
     ${theme.fontStyles.alpha.pSmall}
 
     background-color: ${theme.colors.alpha.white};
+
     border: 1px solid ${theme.colors.alpha.gray_90};
+
+    ${hasError && ` border-bottom:2px solid ${theme.palette.alpha.error}`};
 
     color: ${theme.colors.alpha.gray_300};
 
--- packages/dt-ui-react/components/form/text-area/TextArea.tsx
+++ packages/dt-ui-react/components/form/text-area/TextArea.tsx
@@ -1,9 +1,15 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 
+import { ErrorIcon } from '../../../core/assets';
+import { Box } from '../../box';
 import { Typography } from '../../typography';
 import { LabelField } from '../label-field';
 
-import { TextAreaStyled, TextAreaWrapper } from './TextArea.styled';
+import {
+  InputFieldIconStyled,
+  TextAreaStyled,
+  TextAreaWrapper,
+} from './TextArea.styled';
 
 export interface TextAreaProps {
   label: string;
@@ -12,6 +18,9 @@ export interface TextAreaProps {
   dataTestId?: string;
   value?: string;
   onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  required?: boolean;
+  helperText?: string;
+  hasError?: boolean;
 }
 
 const TextArea = ({
@@ -20,6 +29,9 @@ const TextArea = ({
   dataTestId,
   name,
   value,
+  required,
+  helperText,
+  hasError = false,
   maxLength = 120,
 }: TextAreaProps) => {
   const [chars, setChars] = useState(0);
@@ -50,7 +62,11 @@ const TextArea = ({
     <TextAreaWrapper data-testid={testId}>
       <LabelField isActive={activeInput} forId={testId}>
         {label}
+        {required && '*'}
       </LabelField>
+      {hasError && (
+        <InputFieldIconStyled>{hasError && <ErrorIcon />}</InputFieldIconStyled>
+      )}
       <TextAreaStyled
         value={value}
         name={name || testId}
@@ -59,10 +75,34 @@ const TextArea = ({
         onChange={handleChange}
         onBlur={handleBlur}
         maxLength={maxLength}
+        hasError={hasError}
       />
-      <Typography element='span' fontStyles='pXSmall' color='gray_100'>
-        {maxLength - chars} Characters
-      </Typography>
+      <Box
+        style={{
+          paddingLeft: 15,
+          gap: 16,
+          alignItems: 'baseline',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '100%',
+        }}
+      >
+        <Typography
+          element='span'
+          fontStyles='pXSmall'
+          color={hasError ? 'red_100' : 'gray_100'}
+        >
+          {helperText}
+        </Typography>
+        <Typography
+          style={{ whiteSpace: 'nowrap' }}
+          element='span'
+          fontStyles='pXSmall'
+          color='gray_100'
+        >
+          {maxLength - chars} Characters
+        </Typography>
+      </Box>
     </TextAreaWrapper>
   );
 };
