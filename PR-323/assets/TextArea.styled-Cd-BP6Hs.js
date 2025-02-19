fa7a989fcab247a61efd5e4a19beb81a0f9ddc9d
dt-ui <dt-ui@daimlertruck.com>
2025-02-19 14:23:15 +0000

    chore: update and publish Storybook

---
+++ PR-323/assets/TextArea.styled-Cd-BP6Hs.js
@@ -0,0 +1,68 @@
+import{n as o}from"./emotion-styled.browser.esm-B1le5_Hy.js";const a=(e,r)=>({default:r.palette.surface.default,contrast:r.palette.surface.contrast,light:r.palette.surface.light})[e]||r.palette.surface.default,p=o.div`
+  ${({theme:e})=>`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    position: relative;
+    width: 100%;
+    
+    border-style: solid;
+    border-width: 0;
+    border-color: ${e.palette.border.dark};
+    
+    
+    `}
+`,i=o.div`
+  ${({theme:e})=>`padding-left: ${e.spacing["2xs"]}`};
+`,f=o.div`
+  ${({})=>`
+    display:flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: baseline;
+    width: 100%
+  `}
+`,n=o.textarea`
+  ${({theme:e,variant:r,resize:l,hasError:t=!1,backgroundFill:d})=>`
+    ${e.fontStyles.pSmall}
+
+    background-color: ${a(d,e)};
+
+    width: 100%;
+    padding: 20px 14px 14px 14px;
+    margin-top: 5px;
+    color: ${e.palette.content.dark};
+
+    
+
+   
+
+    border-radius: ${r==="outlined"?e.shape.formField:`${e.shape.formField} ${e.shape.formField} 0 0`};
+
+    border-width: ${r==="outlined"?"1px":"0 0 1px"};
+    
+    outline: none;
+    ${r==="outlined"?`&:focus { border: 1px solid ${e.palette.primary.default};}`:`&:focus { border-bottom: 1px solid ${e.palette.primary.default};}`};
+
+    
+
+    resize: ${l}
+
+
+    ${t&&`
+        ${r} === 'outlined'
+        ? 'border: 2px solid ${e.palette.error.default};}'
+        : 'border-bottom: 2px solid ${e.palette.error.default};}'
+
+        border-color: ${e.palette.error.default};
+
+        ${r==="outlined"?`&:focus { border: 1px solid ${e.palette.error.default};}`:`&:focus { border-bottom: 1px solid ${e.palette.error.default};}`};
+    `};
+
+    border-color: ${t?e.palette.error.default:e.palette.border.default}
+
+    
+  
+  `}
+`;export{p as T,n as a,f as b,i as c};
