bf947eaf24c77fa2a80a0b64c2b65bcba3f56888
dt-ui <dt-ui@daimlertruck.com>
2025-02-20 11:27:51 +0000

    chore: update and publish Storybook

---
+++ PR-329/assets/Form.stories-BmQ6EnhU.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{T as l}from"./TextField-rtq5-I96.js";import"./TextField.styled-BZC2AWJR.js";import{F as t}from"./Form-DO30gIYB.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Icon-DOElHRRq.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";import"./LabelField-D3NXFG_p.js";import"./Typography-B1WrKgOB.js";import"./index-CmU8lkWL.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./Tooltip-CvQZ_ZR6.js";import"./Portal-U_0UEmB3.js";import"./z-index-Ds5QkyqS.js";import"./Info-BPeIL3LD.js";const B={title:"Data Display/Form",component:t},u=()=>r.jsx(t,{children:r.jsx(l,{label:"label 1"})}),o=u.bind({});o.args={};const F=({isDisabled:d,title:c,tooltip:b})=>r.jsxs(t,{children:[r.jsx(t.Group,{title:c,tooltip:b,children:r.jsx(l,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:d,children:[r.jsx(l,{label:"label 2"}),r.jsx(l,{label:"label 3"})]})]}),e=F.bind({});e.args={title:"",tooltip:"",isDisabled:!1};var i,s,a;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  return <Form>
+      <TextField label='label 1' />
+    </Form>;
+}`,...(a=(s=o.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var m,p,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  isDisabled,
+  title,
+  tooltip
+}) => <Form>
+    <Form.Group title={title} tooltip={tooltip}>
+      <TextField label='label 1' />
+    </Form.Group>
+    <Form.Group isDisabled={isDisabled}>
+      <TextField label='label 2' />
+      <TextField label='label 3' />
+    </Form.Group>
+  </Form>`,...(n=(p=e.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const C=["Default","FormGroup"];export{o as Default,e as FormGroup,C as __namedExportsOrder,B as default};
