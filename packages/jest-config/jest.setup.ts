a607d511cd03ee83731be493363d04ba38fafa30
freirea <freirea@tbdir.net>
2025-02-20 11:17:19 +0000

    fix(DOMRect API): prevent breaking consumers CI/CD pipeline

--- packages/jest-config/jest.setup.ts
+++ packages/jest-config/jest.setup.ts
@@ -3,59 +3,6 @@ import '@testing-library/jest-dom';
 import { configure } from '@testing-library/react';
 import './jest-mock-globals';
 
-// Polyfill DOMRect
-if (typeof globalThis.DOMRect === 'undefined') {
-  class DOMRectPolyfill implements DOMRect {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    top: number;
-    left: number;
-    right: number;
-    bottom: number;
-
-    constructor(
-      x: number = 0,
-      y: number = 0,
-      width: number = 0,
-      height: number = 0
-    ) {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.top = y;
-      this.left = x;
-      this.right = x + width;
-      this.bottom = y + height;
-    }
-
-    toJSON(): object {
-      return {
-        x: this.x,
-        y: this.y,
-        width: this.width,
-        height: this.height,
-        top: this.top,
-        left: this.left,
-        right: this.right,
-        bottom: this.bottom,
-      };
-    }
-
-    static fromRect(other?: DOMRectInit): DOMRect {
-      if (other) {
-        const { x = 0, y = 0, width = 0, height = 0 } = other;
-        return new DOMRectPolyfill(x, y, width, height);
-      }
-      return new DOMRectPolyfill();
-    }
-  }
-
-  globalThis.DOMRect = DOMRectPolyfill;
-}
-
 expect.extend(matchers);
 
 configure({ defaultHidden: true });
--- packages/react-packages/tooltip/src/components/content/TooltipContent.tsx
+++ packages/react-packages/tooltip/src/components/content/TooltipContent.tsx
@@ -17,6 +17,38 @@ export interface TooltipContentProps extends BaseProps {
   background?: TooltipBackground;
 }
 
+// Helper to create a DOMRect
+// Usefull for environments where DOMRect is not available.
+const createSafeDOMRect = (): DOMRect => {
+  if (typeof DOMRect !== 'undefined') {
+    return new DOMRect(0, 0, 0, 0);
+  }
+
+  return {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+
+    toJSON() {
+      return {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      };
+    },
+  } as DOMRect;
+};
+
 export const TooltipContent = ({
   children,
   direction = OTooltipDirection.Top,
@@ -27,7 +59,7 @@ export const TooltipContent = ({
   const { isVisible } = useTooltipContext();
   const tooltipRef = useRef<HTMLDivElement>(null);
 
-  const [tooltipRect, setTooltipRect] = useState(new DOMRect(0, 0, 0, 0));
+  const [tooltipRect, setTooltipRect] = useState<DOMRect>(createSafeDOMRect());
 
   useLayoutEffect(() => {
     if (tooltipRef.current) {
