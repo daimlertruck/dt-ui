c2bc8fdc2ad40af3d73bbf834285576ee27cfa05
dt-ui <dt-ui@daimlertruck.com>
2025-03-19 13:45:43 +0000

    chore: update and publish Storybook

---
+++ PR-338/assets/Spinner.stories-Dq1xIaSp.js
@@ -0,0 +1,7 @@
+import{S as i}from"./Spinner-DlKg_IVx.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DMRWov4L.js";import"./Box.styled-TgnH4Hzf.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";import"./Typography-C3Apdm1M.js";import"./index-bKUcOz2Q.js";import"./index-BtM5VmRH.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";import"./Spinner.styled-5cOwHYoC.js";const D={title:"Data Display/Spinner",component:i,parameters:{backgrounds:{values:[{name:"black",value:"#000"}]}},argTypes:{size:{options:["small","medium","large","extra-large"],control:{type:"select"}},colorScheme:{options:["positive","negative"],control:{type:"radio"}}}},e={args:{size:"medium",colorScheme:"positive",loadingText:""}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: 'medium',
+    colorScheme: 'positive',
+    loadingText: ''
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,D as default};
