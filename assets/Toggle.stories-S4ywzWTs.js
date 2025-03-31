077ffdf341b28a1cfe59b2914d8945c2b697a639
dt-ui <dt-ui@daimlertruck.com>
2025-03-31 09:32:48 +0000

    chore: update and publish Storybook

---
+++ assets/Toggle.stories-S4ywzWTs.js
@@ -0,0 +1,8 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{T as r}from"./Toggle-CGHvQv4V.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Typography-D8tl62dX.js";import"./index-DhoK5pdg.js";import"./index-BtM5VmRH.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";const C={title:"Data Display/Toggle",component:r,argTypes:{},render:({...c})=>o.jsx(r,{...c,children:o.jsx(r.Label,{children:"Toggle"})})},e={args:{isChecked:!1,isDisabled:!1,checkedColor:"success",uncheckedColor:"secondary"}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    isChecked: false,
+    isDisabled: false,
+    checkedColor: 'success',
+    uncheckedColor: 'secondary'
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const D=["Default"];export{e as Default,D as __namedExportsOrder,C as default};
