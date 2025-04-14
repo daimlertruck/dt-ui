5fc0b3c910be5d2d16b32b985164ec3ffe655b55
dt-ui <dt-ui@daimlertruck.com>
2025-04-14 16:45:59 +0000

    chore: delete PR preview

--- PR-341/assets/Card.stories-CT_-fH-p.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./jsx-runtime-CKrituN3.js";import{T as m}from"./Typography-ABVy4bD4.js";import{C as t}from"./Card-CG1Xf4EH.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-y-ta-bzz.js";import"./index-BtM5VmRH.js";import"./emotion-styled.browser.esm-B1le5_Hy.js";import"./extends-DJVMZnyl.js";import"./index-BAMY2Nnw.js";import"./util-BQ0cylCP.js";import"./index-BDlDW0Uq.js";const D={title:"Data Display/Card",component:t,render:({title:a,description:p})=>r.jsxs(t,{children:[r.jsx(t.Title,{children:a}),r.jsx(t.Detail,{children:r.jsx(m,{color:"grey_100",children:p})})]})},e={args:{title:"Card Title",description:"This is a short description"}};var i,o,s;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    title: 'Card Title',
-    description: 'This is a short description'
-  }
-}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const y=["Default"];export{e as Default,y as __namedExportsOrder,D as default};
