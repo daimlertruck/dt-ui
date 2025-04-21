16d2849e1290739969e9f6bb05c7f8efd6c76043
dt-ui <dt-ui@daimlertruck.com>
2025-04-21 15:04:25 +0000

    chore: update and publish Storybook

---
+++ PR-352/assets/Dropdown-BstkqMu0.js
@@ -0,0 +1,78 @@
+import{j as o}from"./jsx-runtime-DMAvRu52.js";import{T as $}from"./Typography-C6wkfgHn.js";import{n as g,u as M}from"./emotion-styled.browser.esm-DpDeK1WQ.js";import{r as l}from"./index-Dl6G-zuu.js";import{B as P}from"./index-DECWwmT6.js";import{I as O}from"./Icon-B8gBCtaI.js";import"./index-CTeUJmgI.js";import"./index-DCrStp1_.js";import{a as T}from"./z-index-Ds5QkyqS.js";import{u as z}from"./useClickOutside-CBJSS0tk.js";const A=g.div`
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+  margin-top: 4px;
+`,F=({dataTestId:t,isDisabled:e=!1,hasError:n=!1,children:r=""})=>{const s=e?"content.light":"content.medium";return o.jsx(A,{"data-testid":t??"dropdown-text",children:o.jsx($,{color:n?"error.default":s,element:"span",fontStyles:"body3",children:r})})},h={state:{text:"",value:""},isOpen:!1,setState:()=>null,setIsOpen:()=>null},k=l.createContext(h),N=({children:t,defaultValue:e,name:n})=>{const[r,s]=l.useState(e??h.state),[i,c]=l.useState(h.isOpen);return l.useEffect(()=>e&&s(e),[e]),o.jsx(k.Provider,{value:{state:r,setState:s,isOpen:i,setIsOpen:c,name:n},children:t})},C=()=>{const t=l.useContext(k);if(!t)throw new Error("Dropdown compound components cannot be rendered outside the Dropdown component");return t},R=g.li`
+  ${({theme:t,disabled:e,isSelected:n})=>`
+    ${t.fontStyles[n?"body2Bold":"body2"]};
+    color: ${t.palette.content.default};
+    list-style: none;
+    padding: ${t.spacing["4xs"]} ${t.spacing["2xs"]};
+    text-overflow: ellipsis;
+    overflow-x: hidden;
+
+    &:hover {
+      background: ${e?t.palette.surface.light:t.palette.primary.light};
+      cursor: ${e?"not-allowed":"pointer"};
+    }
+
+    ${e&&`
+        color: ${t.palette.content.light};
+        background: ${t.palette.surface.light};
+      `}
+  `}
+`,U=({dataTestId:t,option:e,children:n,isDisabled:r,onClick:s})=>{const{state:i,setState:c,setIsOpen:f,name:x}=C(),y=t??`dropdown-option-${e.value}`,b={text:e.text??e.value,value:e.value},p=u=>{r||(f(!1),c(b),s&&s(e.value,x,u))};return o.jsx(R,{"data-testid":y,disabled:r,isSelected:i.value===e.value,onClick:p,role:"option",children:n},e.value)},L=g.ul`
+  ${({theme:t})=>`
+    background: ${t.palette.surface.contrast};
+    border: 1px solid ${t.palette.border.default};
+    border-radius: ${t.shape.dropdown};
+    width: 100%;
+    padding:${t.spacing["2xs"]} ${t.spacing.none};
+    margin: ${t.spacing["5xs"]} ${t.spacing.none};
+    position: absolute;
+    right: 0;
+    z-index: ${T};
+    max-height: 180px;
+    overflow: auto;
+  `}
+`,E=({children:t,dataTestId:e,style:n})=>o.jsx(L,{"data-testid":`${e?e+"-":""}menu`,style:n,children:t}),W=(t,e)=>({default:e.palette.surface.default,contrast:e.palette.surface.contrast,light:e.palette.surface.light})[t],X=g.button`
+  ${({theme:t,hasBorder:e=!0,hasError:n=!1,isOpen:r=!1,variant:s="outlined",fill:i="default"})=>{const c=t.palette.border.dark,f=t.palette.content.dark,x=t.palette.error.default;return`
+      background: ${t.palette.surface.contrast};
+      padding: ${t.spacing.xsmall} ${e?t.spacing.xmedium:"0"};
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      text-align: left;
+      width: 100%;
+      height: 53px;
+      cursor: pointer;
+      outline: none;
+
+      background-color: ${W(i,t)};
+
+      border-width: ${s==="outlined"?"1px":"0 0 1px"};
+      border-color: ${r?f:c};
+      border-style: solid;
+
+      &:focus, &:hover {
+        border-color: ${n?x:f};
+      }
+
+      ${n&&`border-color: ${x}`};
+      ${!e&&"border: none"};
+
+      &:disabled {
+        pointer-events: none;
+      }
+    `}}
+`,Z=({children:t,label:e,style:n,dataTestId:r="dropdown-select",isDisabled:s=!1,isRequired:i,hasBorder:c=!0,hasError:f=!1,hasDeselect:x=!1,variant:y="outlined",fill:b="default"})=>{const{state:p,setState:u,isOpen:m,setIsOpen:I}=C(),S=M(),D=l.Children.count(t)===1,w=s||D,j=w?S.palette.content.light:S.palette.content.default,_=a=>{a.preventDefault(),I(d=>!d)},B=a=>{a.stopPropagation(),u({text:"",value:""})};return l.useEffect(()=>{l.Children.toArray(t).find(d=>d.props.option.value===p.value)||u({text:"",value:""})},[t,u,p.value]),l.useEffect(()=>{if(D){const a=l.Children.map(t,d=>d&&d.props.option);if(a&&a[0]){const d={text:a[0].text??a[0].value,value:a[0].value};u(d)}}},[D,u,t]),o.jsxs(o.Fragment,{children:[o.jsxs(X,{"data-testid":r,disabled:w,fill:b,hasBorder:c,hasError:f,isOpen:m,onClick:_,style:n,variant:y,children:[o.jsxs("div",{style:{overflow:"hidden"},children:[o.jsxs($,{color:w?"content.light":"content.default",fontStyles:"body3",children:[e,i?o.jsx($,{color:"error.default",element:"span",fontStyles:"body3",children:"*"}):null]}),o.jsx($,{color:w?"content.light":"content.default",fontStyles:"body2",style:{textOverflow:"ellipsis",overflow:"hidden"},children:p.value?p.text:"Select an option"})]}),o.jsxs(P,{style:{flexDirection:"row",gap:"0.5rem"},children:[x&&p.value?o.jsx(O,{code:"close",color:j,dataTestId:"deselect-value",onClick:B,size:"medium"}):null,o.jsx(O,{code:m?"expand_less":"expand_more",color:j,size:"large"})]})]}),m?o.jsx(E,{dataTestId:r,children:t}):null]})},q=({children:t,style:e,dataTestId:n})=>{const{setIsOpen:r}=C(),s=l.useRef(null);return z({ref:s,handler:()=>r(!1)}),o.jsx("div",{"data-testid":n,ref:s,style:e,children:t})},G=g.div`
+  ${({theme:t,style:e})=>`
+    margin: ${t.spacing.none};
+    display: inline-block;
+    position: relative;
+    width: 100%;
+    ${e}
+  `}
+`,v=({children:t,defaultValue:e,style:n,name:r,dataTestId:s="dropdown"})=>o.jsx(N,{defaultValue:e,name:r,children:o.jsx(G,{"data-testid":s,role:"menu",style:n,children:t})});v.Container=q;v.Detail=F;v.Select=Z;v.Option=U;v.Menu=E;export{v as D};
---
+++ PR-352/assets/preview-JhfmPkep.js
@@ -0,0 +1,33 @@
+import{j as i}from"./jsx-runtime-DMAvRu52.js";import{e as _,g as S,G as d}from"./index-CTeUJmgI.js";import{_ as P}from"./index-D-mPsPJ7.js";import{d as R}from"./index-DrFu-skq.js";import{R as r}from"./index-Dl6G-zuu.js";import{t as h}from"./index-B3ZVsDqP.js";import{T as x}from"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-DCrStp1_.js";import{a as F,t as O}from"./index-YcDUnvpr.js";import"./index-BAMY2Nnw.js";import"./iframe-CDQhpt_Q.js";import"./IconButton-DwSkHMft.js";import"./Link-Cm82UH6r.js";import"./Box.styled-Bx5NRrR7.js";import"./Typography-C6wkfgHn.js";import"./Spinner.styled-DXh-wD4k.js";import"./Button.styled-C8Rd_00g.js";import"./AccordionBody.styled-CR-1yGH3.js";import"./Tooltip-ptca3QAh.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Avatar.styled-TZ4ZdhzH.js";import"./Backdrop-BNK3Rtxy.js";import"./Breadcrumb-BwlJ75mX.js";import"./useMedia-D3XZz7EL.js";import"./useDebounceResize-DIfR_9YA.js";import"./debounce-BF4Cdv7I.js";import"./Divider.styled-Cm1mEvej.js";import"./Drawer-Coqqx94D.js";import"./Close-mVzWqPf0.js";import"./EmptyState-CSaeLuNv.js";import"./Icon-B8gBCtaI.js";import"./LabelField-pJcXPDNt.js";import"./Message-5uJCxihf.js";import"./Info-BRKcLzcc.js";import"./Tabs-CxG-LV3M.js";import"./index-DECWwmT6.js";import"./Tag-DarPqZ64.js";import"./index-ahf9P0Fa.js";import"./Checkbox-DCu8g_Y2.js";import"./TextField.styled-D_btMrKR.js";import"./Table-CFHt8H9-.js";import"./ProgressBar.styled-m8XzoYja.js";import"./Select-BxonAwqw.js";import"./useClickOutside-CBJSS0tk.js";import"./Step-lbz9EcKZ.js";import"./Radio.styled-yyYnSofk.js";import"./Modal-C41p88gi.js";import"./Spinner-BnNimsI2.js";import"./Card-CeWIXSrP.js";import"./SegmentedControl-CnbDndzu.js";import"./Dropdown-BstkqMu0.js";import"./Toggle-C2qUUvP8.js";import"./TextArea.styled-DQv77iaI.js";import"./Form-BNfWH6Pg.js";import"./Pagination-MMyWL00P.js";const D=o=>S`
+  ${_}
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: greyscale;
+    font-family: ${o.fontFamily.default};
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${o.fontFamily.default};
+    width: 100%;
+    min-height: 100vh;
+
+    ${o.fontStyles.body1}
+  }
+
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
+`,A=({theme:o=h,webFonts:t=[],children:e})=>i.jsxs(i.Fragment,{children:[i.jsx(d,{styles:[D(o),...t]}),i.jsx(x,{theme:o,children:e})]}),{useParameter:C,addons:G,useEffect:Xo,useMemo:I,definePreview:Zo}=__STORYBOOK_MODULE_PREVIEW_API__,{deprecate:M}=__STORYBOOK_MODULE_CLIENT_LOGGER__;var W=Object.defineProperty,b=(o,t)=>{for(var e in t)W(o,e,{get:t[e],enumerable:!0})},k={};b(k,{initialGlobals:()=>L});var n="themes",j=`storybook/${n}`,y="theme",u={},$={REGISTER_THEMES:`${j}/REGISTER_THEMES`},L={[y]:""},N={};b(N,{initializeThemeState:()=>g,pluckThemeFromContext:()=>w,useThemeParameters:()=>z});function w({globals:o}){return o[y]||""}function z(o){return M(R`The useThemeParameters function is deprecated. Please access parameters via the context directly instead e.g.
+    - const { themeOverride } = context.parameters.themes ?? {};
+    `),o?o.parameters[n]??u:C(n,u)}function g(o,t){G.getChannel().emit($.REGISTER_THEMES,{defaultTheme:t,themes:o})}var H=([o,t])=>t,K=({Provider:o,GlobalStyles:t,defaultTheme:e,themes:a={}})=>{let E=Object.keys(a),f=e;return g(E,f),(m,p)=>{let{themeOverride:s}=p.parameters[n]??{},l=w(p),T=I(()=>{let v=s||l||f,c=Object.entries(a);return c.length===1?H(c[0]):a[v]},[l,s]);return o?r.createElement(o,{theme:T},t&&r.createElement(t,null),m()):r.createElement(r.Fragment,null,t&&r.createElement(t,null),m())}};const B=[{"@font-face":{fontFamily:"Roboto",fontStyle:"normal",fontWeight:400,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-regular.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-regular.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Roboto",fontStyle:"italic",fontWeight:400,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Roboto",fontStyle:"normal",fontWeight:900,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-900.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-900.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:400,src:"url(/fonts/poppins/poppins-regular.woff) format('woff'), url(/fonts/poppins/poppins-regular.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"italic",fontWeight:400,src:"url(/fonts/poppins/poppins-italic.woff) format('woff'), url(/fonts/poppins/poppins-italic.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:600,src:"url(/fonts/poppins/poppins-semi-bold.woff) format('woff'), url(/fonts/poppins/poppins-semi-bold.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:700,src:"url(/fonts/poppins/poppins-bold.woff) format('woff'), url(/fonts/poppins/poppins-bold.woff2) format('woff2')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"normal",fontWeight:400,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Regular.otf) format('opentype')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"normal",fontWeight:700,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Bold.otf) format('opentype')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"italic",fontWeight:400,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Italic.otf) format('opentype')"}}],Y=()=>i.jsx(d,{styles:[...B]}),qo=[K({themes:{Default:h,Greenlane:O,TruckAPI:F},defaultTheme:"Default",Provider:A,GlobalStyles:Y})],Qo={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},viewMode:"docs",previewTabs:{"storybook/docs/panel":{index:-1},canvas:{title:"Sandbox"}},options:{storySort:{order:["Contribution Guidelines","Foundations","*"]}},docs:{theme:P.light}},tags:["autodocs"]};export{qo as decorators,Qo as default};
