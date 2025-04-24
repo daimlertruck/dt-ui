5eefb3354facd89fd6e861a8206358e2e997e92e
dt-ui <dt-ui@daimlertruck.com>
2025-04-24 11:58:19 +0000

    chore: update and publish Storybook

---
+++ PR-357/assets/Select.stories-D3A08qgP.js
@@ -0,0 +1,13 @@
+import{j as l}from"./jsx-runtime-DMAvRu52.js";import{S as s}from"./Select-BGvz8wzu.js";import"./index-Dl6G-zuu.js";import"./index-DkNsIvS9.js";import"./index-DCrStp1_.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-BAMY2Nnw.js";import"./iframe-BDxjkUgW.js";import"./useClickOutside-CBJSS0tk.js";import"./Icon-B8gBCtaI.js";import"./LabelField-M7z5_IE0.js";import"./Typography-Cz1wMlAm.js";import"./Tooltip-BtB8CvIx.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Checkbox-DCu8g_Y2.js";const d=[{value:"opt1",label:"Option 1",disabled:!1},{value:"opt2",label:"Option 2",disabled:!1},{value:"opt3",label:"A text message to see how big labels will be handled",disabled:!1},{value:"opt4",label:"Option 3",disabled:!0},{value:"opt5",label:"Option 4",disabled:!1}],R={title:"Data Display/Select",component:s,argTypes:{},render:({options:i,...p})=>l.jsx("div",{style:{height:"500px",padding:"10px",backgroundColor:"#EBECED"},children:l.jsx(s,{...p,children:i.map((e,n)=>l.jsx(s.Option,{disabled:e.disabled,index:n,label:e.label,value:e.value},e.value))})})},a={args:{options:d,label:"My label",helperText:"Some message",errorMessage:"",isMulti:!1,isRequired:!1,hasError:!1,isDisabled:!1,initialValue:""}};var o,r,t;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    options: options,
+    label: 'My label',
+    helperText: 'Some message',
+    errorMessage: '',
+    isMulti: false,
+    isRequired: false,
+    hasError: false,
+    isDisabled: false,
+    initialValue: ''
+  }
+}`,...(t=(r=a.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const T=["Default"];export{a as Default,T as __namedExportsOrder,R as default};
