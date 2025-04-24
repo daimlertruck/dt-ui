89c02ee7ec4ef25c18ddff8aa723cb471b109617
dt-ui <dt-ui@daimlertruck.com>
2025-04-24 11:52:57 +0000

    chore: delete PR preview

--- PR-356/assets/Link.stories-BWpiQPsD.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./jsx-runtime-DMAvRu52.js";import{L as i,a as p}from"./Link-BNCFMxvB.js";import"./index-Dl6G-zuu.js";import"./Box.styled-Bx5NRrR7.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./Typography-DK9Xhyyn.js";import"./index-DH-DdQKk.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-B46FW0pZ.js";import"./Spinner.styled-RENjdYgd.js";import"./Button.styled-DrlIsltv.js";const b={title:"Data Display/Link",component:i},n=({...s})=>a.jsx(i,{...s}),r=n.bind({});r.args={children:"Redirect to",href:"https://www.google.com/",target:"_blank",textSize:p.Medium,isUnderline:!0,isDisabled:!1};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`({
-  ...props
-}) => {
-  return <Link {...props} />;
-}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,b as default};
