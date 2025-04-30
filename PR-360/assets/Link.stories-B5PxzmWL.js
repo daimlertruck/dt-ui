2aa97772119513d641411e0feabb8477be2bed07
dt-ui <dt-ui@daimlertruck.com>
2025-04-30 10:25:25 +0000

    chore: update and publish Storybook

---
+++ PR-360/assets/Link.stories-B5PxzmWL.js
@@ -0,0 +1,11 @@
+import{j as s}from"./jsx-runtime-DMAvRu52.js";import{L as t,a}from"./enum-GvtM6JQl.js";import"./index-Dl6G-zuu.js";import"./Box.styled-Bx5NRrR7.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./Typography-DunYvYht.js";import"./index-BRLac9hs.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-Bb4CqhTw.js";import"./Spinner.styled-U2rS-ba1.js";import"./Button.styled-Bp3ur7y4.js";const l=["button","text"],m=["small","medium","large"],k={component:t,title:"Data Display/Link",argTypes:{children:{control:"text"},target:{control:"text"},href:{control:"text"},textSize:{options:m,control:{type:"select"}},type:{options:l,control:{type:"select"}},isUnderline:{control:{type:"boolean"},defaultValue:{summary:!1},description:"Add bottom border to Header"},isDisabled:{control:{type:"boolean"},defaultValue:{summary:!1},description:"Add bottom border to Header"}},render:({...n})=>s.jsx(t,{...n})},e={args:{children:"Redirect to",href:"https://www.google.com/",target:"_blank",textSize:a.Medium,isUnderline:!0,isDisabled:!1,type:"text"}};var o,r,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Redirect to',
+    href: 'https://www.google.com/',
+    target: '_blank',
+    textSize: LinkTextSize.Medium,
+    isUnderline: true,
+    isDisabled: false,
+    type: 'text'
+  }
+}`,...(i=(r=e.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const w=["Default"];export{e as Default,w as __namedExportsOrder,k as default};
