54aaebd258c9e6b238171494253a268e5b657da2
dt-ui <dt-ui@daimlertruck.com>
2025-05-02 11:39:12 +0000

    chore: update and publish Storybook

---
+++ PR-363/assets/Link.stories-CDE56W8S.js
@@ -0,0 +1,5 @@
+import{j as a}from"./jsx-runtime-DMAvRu52.js";import{L as i,a as p}from"./Link-D7JNuPcE.js";import"./index-Dl6G-zuu.js";import"./Box.styled-Bx5NRrR7.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./Typography-DEIXEsHv.js";import"./index-DuKeXE76.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-DjoflINV.js";import"./Spinner.styled-uqYu2Nls.js";import"./Button.styled-Bp3ur7y4.js";const b={title:"Data Display/Link",component:i},n=({...s})=>a.jsx(i,{...s}),r=n.bind({});r.args={children:"Redirect to",href:"https://www.google.com/",target:"_blank",textSize:p.Medium,isUnderline:!0,isDisabled:!1};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`({
+  ...props
+}) => {
+  return <Link {...props} />;
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,b as default};
