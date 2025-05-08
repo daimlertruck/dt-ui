8f623b18e55491d79671554d28dbd5e34b021781
dt-ui <dt-ui@daimlertruck.com>
2025-05-08 16:01:23 +0000

    chore: update and publish Storybook

---
+++ PR-366/assets/Typography.stories-CTuZRvdf.js
@@ -0,0 +1,11 @@
+import{T as t}from"./Typography-D8qrIIPH.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./index-CeGmCZh1.js";import"./index-DCrStp1_.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-BAMY2Nnw.js";import"./iframe-8ZqOtPh6.js";const _=["h1","h2","h3","h4","h5","h6","p","span","label","a"],p=["h1","h2","h3","h4","h5","h6","body1Bold","body1","body2Bold","body2","body3Bold","body3","body4Bold","body4","link1Bold","link1","link2Bold","link2","link3Bold","link3","button1","button2","button3","p","pSmall","pXSmall","pXXSmall","tag"],n=["black","blue","blue_05","blue_10","blue_20","blue_30","blue_40","blue_50","blue_60","blue_70","blue_80","blue_90","grey_05","grey_10","grey_20","grey_30","grey_40","grey_50","grey_60","grey_70","grey_80","grey_90","grey_100","green","green_05","green_10","green_20","green_30","green_40","green_50","green_60","green_70","green_80","green_90","red","red_05","red_10","red_20","red_30","red_40","red_50","red_60","red_70","red_80","red_90","white","yellow","yellow_05","yellow_10","yellow_20","yellow_30","yellow_40","yellow_50","yellow_60","yellow_70","yellow_80","yellow_90","purple","purple_05","purple_10","purple_20","purple_30","purple_40","purple_50","purple_60","purple_70","purple_80","purple_90"],c={title:"Data Display/Typography",component:t,argTypes:{element:{options:_,control:"select"},fontStyles:{options:p,control:"select"},color:{options:n,control:"select"}}},e={args:{id:"MyID",element:"h1",fontStyles:"h1",color:"grey_100",children:"Example text",dataTestId:"typography-story-id",style:{}}};var l,r,o;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    id: 'MyID',
+    element: 'h1',
+    fontStyles: 'h1',
+    color: 'grey_100',
+    children: 'Example text',
+    dataTestId: 'typography-story-id',
+    style: {}
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,c as default};
