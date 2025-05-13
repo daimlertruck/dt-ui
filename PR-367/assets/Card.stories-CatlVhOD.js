0ae68d64cb4cfa2d7e0d15341677d2fc96ce66d8
dt-ui <dt-ui@daimlertruck.com>
2025-05-13 09:38:31 +0000

    chore: update and publish Storybook

---
+++ PR-367/assets/Card.stories-CatlVhOD.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-DMAvRu52.js";import{T as n}from"./Typography-BfE0fi2Q.js";import{C as t}from"./Card-BYedrIS0.js";import"./index-Dl6G-zuu.js";import"./index-Dqb7-rHP.js";import"./index-DCrStp1_.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-BAMY2Nnw.js";import"./iframe-CdQaNi7c.js";const j={title:"Data Display/Card",component:t,render:({title:a,description:p})=>r.jsxs(t,{children:[r.jsx(t.Title,{children:a}),r.jsx(t.Detail,{children:r.jsx(n,{color:"grey_100",children:p})})]})},e={args:{title:"Card Title",description:"This is a short description"}};var s,i,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: 'Card Title',
+    description: 'This is a short description'
+  }
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,j as default};
