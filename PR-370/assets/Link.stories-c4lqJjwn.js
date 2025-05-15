47e5fc836b2579b55bacab4c9ee7ac798c033fa3
dt-ui <dt-ui@daimlertruck.com>
2025-05-15 16:08:29 +0000

    chore: delete PR preview

--- PR-370/assets/Link.stories-c4lqJjwn.js
+++
@@ -1,9 +0,0 @@
-import{j as i}from"./jsx-runtime-DMAvRu52.js";import{L as t,a as n}from"./enum-BMjH03sM.js";import"./index-Dl6G-zuu.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";const l=["small","medium","large"],u={component:t,title:"Data Display/Link",argTypes:{children:{control:"text"},target:{control:"text"},href:{control:"text"},textSize:{options:l,control:{type:"select"},description:"Sets the size of text"},isDisabled:{control:{type:"boolean"},defaultValue:{summary:!1},description:"Sets the link disabled"}},render:({...a})=>i.jsx(t,{...a})},e={args:{children:"Redirect to",href:"https://www.google.com/",target:"_blank",textSize:n.Medium,isDisabled:!1}};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Redirect to',
-    href: 'https://www.google.com/',
-    target: '_blank',
-    textSize: LinkTextSize.Medium,
-    isDisabled: false
-  }
-}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,u as default};
