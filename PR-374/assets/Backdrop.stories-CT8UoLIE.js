7462ebbfec3b387a365052c185af524cea59a704
dt-ui <dt-ui@daimlertruck.com>
2025-05-21 08:20:38 +0000

    chore: update and publish Storybook

---
+++ PR-374/assets/Backdrop.stories-CT8UoLIE.js
@@ -0,0 +1,7 @@
+import{j as e}from"./jsx-runtime-DMAvRu52.js";import"./index-C3wumSGJ.js";import{P as d}from"./Portal-DcNaNmD6.js";import{r as m}from"./index-Dl6G-zuu.js";import{B as a,a as c}from"./Backdrop-WQIjRJpR.js";import"./index-DCrStp1_.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-BAMY2Nnw.js";import"./iframe-CKlHZC9W.js";import"./z-index-Ds5QkyqS.js";const u=({...r})=>{const[o,l]=m.useState(!1),n=()=>l(!o);return e.jsxs(e.Fragment,{children:[e.jsx("button",{onClick:n,type:"button",children:"Open Backdrop"}),e.jsx(d,{isOpen:o,children:e.jsxs(c,{...r,isOpen:o,children:[e.jsx(e.Fragment,{children:r.children}),e.jsx("button",{onClick:n,type:"button",children:"Close Backdrop"})]})})]})},t={args:{isOpen:!1,type:a.Dark,children:e.jsx("div",{children:"Example content."})}},D={title:"Data Display/Backdrop",component:c,render:u,argTypes:{type:{mapping:a,options:Object.values(a).filter(r=>typeof r=="string"),control:{type:"inline-radio"}},children:{table:{disable:!0}},isOpen:{table:{disable:!0}}}};var s,p,i;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    isOpen: false,
+    type: BackdropType.Dark,
+    children: <div>Example content.</div>
+  }
+}`,...(i=(p=t.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const E=["Default"];export{t as Default,E as __namedExportsOrder,D as default};
