506970b92006b4cab196adf238d7916f9b0cbfff
dt-ui <dt-ui@daimlertruck.com>
2025-05-21 10:41:01 +0000

    chore: update and publish Storybook

---
+++ assets/Spinner.stories-DeUaYKMO.js
@@ -0,0 +1,7 @@
+import{S as a}from"./Spinner-BZUekR3z.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./index-LNaMzCj1.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./Typography-HP1e0ppk.js";import"./index-BV23wApg.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-BUZTVs5N.js";const S={title:"Data Display/Spinner",component:a,parameters:{backgrounds:{values:[{name:"black",value:"#000"}]}},argTypes:{size:{options:["extra-small","small","medium","large","extra-large"],control:{type:"select"}},colorScheme:{options:["positive","negative"],control:{type:"radio"}}}},e={args:{size:"medium",colorScheme:"positive",loadingText:""}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: 'medium',
+    colorScheme: 'positive',
+    loadingText: ''
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,S as default};
