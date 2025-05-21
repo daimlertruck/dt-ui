f327040283a101a9f0cbfedb62d06a43ce8a3dc1
dt-ui <dt-ui@daimlertruck.com>
2025-05-21 10:49:57 +0000

    chore: update and publish Storybook

---
+++ PR-373/assets/LabelField.stories-CivsCIQ9.js
@@ -0,0 +1,10 @@
+import{j as l}from"./jsx-runtime-DMAvRu52.js";import{L as r}from"./LabelField-95jJSu-C.js";import"./index-Dl6G-zuu.js";import"./Typography-BAuyNnW0.js";import"./index-7_eZGw1X.js";import"./index-DCrStp1_.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-BAMY2Nnw.js";import"./iframe-D2mDDuta.js";const D={title:"Data Display/LabelField",component:r,argTypes:{},render:({children:t,...o})=>l.jsx(r,{...o,children:t})},e={args:{children:"My Label",isActive:!1,isDisabled:!1,isRequired:!1,hasError:!1,isFloating:!0}};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'My Label',
+    isActive: false,
+    isDisabled: false,
+    isRequired: false,
+    hasError: false,
+    isFloating: true
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,D as default};
