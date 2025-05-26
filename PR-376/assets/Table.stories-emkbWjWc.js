4adb54a96adde2108cef7d7083fe6eac40dc064e
dt-ui <dt-ui@daimlertruck.com>
2025-05-26 15:38:19 +0000

    chore: update and publish Storybook

---
+++ PR-376/assets/Table.stories-emkbWjWc.js
@@ -0,0 +1,46 @@
+import{j as e}from"./jsx-runtime-DMAvRu52.js";import{I as C}from"./index-CVJBqEtD.js";import"./index-Dl6G-zuu.js";import"./AccordionBody.styled-BsWtaJvH.js";import"./Icon-B8gBCtaI.js";import{A as w}from"./Avatar-BMUlsvXw.js";import{a as y,A as b}from"./Avatar.styled-0Y8bcylJ.js";import"./Backdrop-CXJkX_aB.js";import"./index-LNaMzCj1.js";import"./Breadcrumb-BbDCMb46.js";import"./index-JfbAVmEK.js";import"./index-DCrStp1_.js";import"./Divider.styled-Cm1mEvej.js";import"./Drawer-BE23ZXn3.js";import"./EmptyState-cg5oUgMX.js";import"./Typography-Cw1p2Rqw.js";import"./LabelField-CmmG49T0.js";import"./Message-BRKdmaY-.js";import"./Spinner-DUJpXnKm.js";import"./Tabs-DNxHcvz0.js";import{T as v}from"./Tag-DfnDmjtI.js";import{T as s}from"./Tooltip-MQQMCWiD.js";import"./Checkbox-DCu8g_Y2.js";import"./TextField.styled-D_btMrKR.js";import{T as t}from"./Table-CO4-QK0M.js";import"./Button.styled-Bp3ur7y4.js";import"./ProgressBar.styled-m8XzoYja.js";import"./Select-Tc3ESeeI.js";import"./Step-FRUXvsh6.js";import"./Radio.styled-yyYnSofk.js";import"./Modal-Cih7JQnB.js";import"./Card-DRriMOdQ.js";import"./SegmentedControl-Dv1Rb0Sv.js";import"./Dropdown-CPv0ux_c.js";import"./Toggle.styled-D98PeUM5.js";import"./TextArea.styled-BPVqD145.js";import"./Form-6aSL_GFU.js";import"./Pagination-Dm7clnMN.js";import{I as A}from"./IconButton-k4gaNOO5.js";import"./enum-BNA-F9js.js";import"./Close-mVzWqPf0.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./z-index-Ds5QkyqS.js";import"./useMedia-D3XZz7EL.js";import"./useDebounceResize-BBOCk9Bj.js";import"./debounce-BF4Cdv7I.js";import"./index-BAMY2Nnw.js";import"./iframe-TTJFbHmp.js";import"./Info-BRKcLzcc.js";import"./Portal-DcNaNmD6.js";import"./useClickOutside-CBJSS0tk.js";const D=n=>e.jsx("svg",{fill:"currentColor",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg",...n,children:e.jsx("path",{d:"M12 8C13.1 8 14 7.1 14 6C14 4.9 13.1 4 12 4C10.9 4 10 4.9 10 6C10 7.1 10.9 8 12 8ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10ZM12 16C10.9 16 10 16.9 10 18C10 19.1 10.9 20 12 20C13.1 20 14 19.1 14 18C14 16.9 13.1 16 12 16Z"})}),i=["Name","Email","Role","Status","Phone","Department","Location","Actions"],T=[["John Doe","john@example.com","Developer","green","+123456789","Engineering","New York",""],["Jane Smith","jane@example.com","Designer","grey","+987654321","Design","San Francisco",""],["Alice Johnson","alice@example.com","Manager","yellow","+135792468","Management","Los Angeles",""],["Bob Brown","bob@example.com","Engineer","green","+246813579","Engineering","Chicago",""]],j={Name:"200px",Status:"120px",Actions:"fit-content"},S=n=>e.jsxs("div",{style:{display:"flex",alignItems:"center",gap:"8px"},children:[e.jsx(w,{size:b.Small,title:n,type:y.Primary}),n]}),I=n=>e.jsx(v,{border:"rounded",color:n,size:"small",children:{green:"ACTIVE",yellow:"PENDING",grey:"DEACTIVATED"}[n]}),f=(n,r)=>{const o=i[n];return{Name:S(r),Status:I(r),Actions:E()}[o]??r},E=()=>e.jsx(A,{children:e.jsx(D,{})}),Ee={title:"Data Display/Table",component:t,render:({isFixed:n,hasFixedHeader:r})=>e.jsx("div",{style:{...r&&{height:"200px",overflow:"auto"}},children:e.jsxs(t,{isFixed:n,children:[e.jsx(t.Head,{hasFixedHeader:r,children:e.jsx(t.Row,{children:i.map(o=>e.jsx(t.ColumnHeader,{dataTestId:"test",children:e.jsxs("div",{style:{display:"flex",alignItems:"center",gap:4},children:[o,o==="Status"&&e.jsxs(s,{children:[e.jsx(C,{height:16,width:16}),e.jsx(s.Content,{children:"Pending: Yellow | Active: Green | Deactivated: Grey"})]})]})},`column-header-${o}`))})}),e.jsx(t.Body,{children:T.map(o=>e.jsx(t.Row,{children:o.map((m,d)=>e.jsx(t.DataCell,{columnWidth:j[i[d]],children:f(d,m)},`column-${i[d]}-${m}`))},o.toString()))})]})})},a={args:{isFixed:!1,hasFixedHeader:!1}},R=1,B=2,l={title:"Table",component:t,render:()=>e.jsx("div",{style:{width:"500px",overflow:"auto"},children:e.jsxs(t,{fixedColumnCount:R,fixedEndColumnCount:B,children:[e.jsx(t.Head,{children:e.jsx(t.Row,{children:i.map(n=>e.jsx(t.ColumnHeader,{dataTestId:"test",children:e.jsxs("div",{style:{display:"flex",alignItems:"center",gap:4},children:[n,n==="Status"&&e.jsxs(s,{children:[e.jsx(C,{height:16,width:16}),e.jsx(s.Content,{children:"Pending: Yellow | Active: Green | Deactivated: Grey"})]})]})},`column-header-${n}`))})}),e.jsx(t.Body,{children:T.map(n=>e.jsx(t.Row,{children:n.map((r,o)=>e.jsx(t.DataCell,{columnWidth:j[i[o]],style:{...i[o]==="Actions"&&{minWidth:"65px"}},children:f(o,r)},`column-${i[o]}-${r}`))},n.toString()))})]})})};var c,p,u;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    isFixed: false,
+    hasFixedHeader: false
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var h,x,g;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  title: 'Table',
+  component: Table,
+  render: () => <div style={{
+    width: '500px',
+    overflow: 'auto'
+  }}>
+      <Table fixedColumnCount={fixedColumnCount} fixedEndColumnCount={fixedEndColumnCount}>
+        <Table.Head>
+          <Table.Row>
+            {columns.map((column: string) => <Table.ColumnHeader dataTestId='test' key={\`column-header-\${column}\`}>
+                <div style={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 4
+            }}>
+                  {column}
+                  {column === 'Status' && <Tooltip>
+                      <InfoOutlineIcon height={16} width={16} />
+                      <Tooltip.Content>
+                        Pending: Yellow | Active: Green | Deactivated: Grey
+                      </Tooltip.Content>
+                    </Tooltip>}
+                </div>
+              </Table.ColumnHeader>)}
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {rows.map((row: string[]) => <Table.Row key={row.toString()}>
+              {row.map((content: string, i: number) => <Table.DataCell columnWidth={columnSizes[columns[i]]} key={\`column-\${columns[i]}-\${content}\`} style={{
+            ...(columns[i] === 'Actions' && {
+              minWidth: '65px'
+            })
+          }}>
+                  {renderContent(i, content)}
+                </Table.DataCell>)}
+            </Table.Row>)}
+        </Table.Body>
+      </Table>
+    </div>
+}`,...(g=(x=l.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};const Re=["Default","TableWithPinnedColumns"];export{a as Default,l as TableWithPinnedColumns,Re as __namedExportsOrder,Ee as default};
---
+++ PR-376/assets/preview-BGoGWhxa.js
@@ -0,0 +1,33 @@
+import{j as i}from"./jsx-runtime-DMAvRu52.js";import{e as _,g as S,G as d}from"./index-JfbAVmEK.js";import{_ as P}from"./index-C0xVKLM6.js";import{d as R}from"./index-DrFu-skq.js";import{R as r}from"./index-Dl6G-zuu.js";import{t as h}from"./index-CIrUiNih.js";import{T as x}from"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-DCrStp1_.js";import{a as F,t as O}from"./index-DXBEoNNW.js";import"./index-BAMY2Nnw.js";import"./iframe-TTJFbHmp.js";import"./AccordionBody.styled-BsWtaJvH.js";import"./Icon-B8gBCtaI.js";import"./Tooltip-MQQMCWiD.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Avatar.styled-0Y8bcylJ.js";import"./Backdrop-CXJkX_aB.js";import"./index-LNaMzCj1.js";import"./Breadcrumb-BbDCMb46.js";import"./useMedia-D3XZz7EL.js";import"./useDebounceResize-BBOCk9Bj.js";import"./debounce-BF4Cdv7I.js";import"./Divider.styled-Cm1mEvej.js";import"./Drawer-BE23ZXn3.js";import"./Typography-Cw1p2Rqw.js";import"./IconButton-k4gaNOO5.js";import"./Close-mVzWqPf0.js";import"./EmptyState-cg5oUgMX.js";import"./LabelField-CmmG49T0.js";import"./Message-BRKdmaY-.js";import"./Info-BRKcLzcc.js";import"./Spinner-DUJpXnKm.js";import"./Tabs-DNxHcvz0.js";import"./Tag-DfnDmjtI.js";import"./index-CVJBqEtD.js";import"./Checkbox-DCu8g_Y2.js";import"./TextField.styled-D_btMrKR.js";import"./Table-CO4-QK0M.js";import"./Button.styled-Bp3ur7y4.js";import"./ProgressBar.styled-m8XzoYja.js";import"./Select-Tc3ESeeI.js";import"./useClickOutside-CBJSS0tk.js";import"./Step-FRUXvsh6.js";import"./Radio.styled-yyYnSofk.js";import"./Modal-Cih7JQnB.js";import"./Card-DRriMOdQ.js";import"./SegmentedControl-Dv1Rb0Sv.js";import"./Dropdown-CPv0ux_c.js";import"./Toggle.styled-D98PeUM5.js";import"./TextArea.styled-BPVqD145.js";import"./Form-6aSL_GFU.js";import"./Pagination-Dm7clnMN.js";import"./enum-BNA-F9js.js";const D=o=>S`
+  ${_}
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: greyscale;
+    font-family: ${o.fontFamily.default};
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${o.fontFamily.default};
+    width: 100%;
+    min-height: 100vh;
+
+    ${o.fontStyles.body1}
+  }
+
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
+`,A=({theme:o=h,webFonts:t=[],children:e})=>i.jsxs(i.Fragment,{children:[i.jsx(d,{styles:[D(o),...t]}),i.jsx(x,{theme:o,children:e})]}),{useParameter:C,addons:G,useEffect:Vo,useMemo:I,definePreview:Jo}=__STORYBOOK_MODULE_PREVIEW_API__,{deprecate:M}=__STORYBOOK_MODULE_CLIENT_LOGGER__;var W=Object.defineProperty,b=(o,t)=>{for(var e in t)W(o,e,{get:t[e],enumerable:!0})},k={};b(k,{initialGlobals:()=>L});var n="themes",j=`storybook/${n}`,y="theme",u={},$={REGISTER_THEMES:`${j}/REGISTER_THEMES`},L={[y]:""},N={};b(N,{initializeThemeState:()=>g,pluckThemeFromContext:()=>w,useThemeParameters:()=>z});function w({globals:o}){return o[y]||""}function z(o){return M(R`The useThemeParameters function is deprecated. Please access parameters via the context directly instead e.g.
+    - const { themeOverride } = context.parameters.themes ?? {};
+    `),o?o.parameters[n]??u:C(n,u)}function g(o,t){G.getChannel().emit($.REGISTER_THEMES,{defaultTheme:t,themes:o})}var H=([o,t])=>t,K=({Provider:o,GlobalStyles:t,defaultTheme:e,themes:a={}})=>{let E=Object.keys(a),f=e;return g(E,f),(m,p)=>{let{themeOverride:s}=p.parameters[n]??{},l=w(p),T=I(()=>{let v=s||l||f,c=Object.entries(a);return c.length===1?H(c[0]):a[v]},[l,s]);return o?r.createElement(o,{theme:T},t&&r.createElement(t,null),m()):r.createElement(r.Fragment,null,t&&r.createElement(t,null),m())}};const B=[{"@font-face":{fontFamily:"Roboto",fontStyle:"normal",fontWeight:400,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-regular.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-regular.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Roboto",fontStyle:"italic",fontWeight:400,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Roboto",fontStyle:"normal",fontWeight:900,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-900.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-900.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:400,src:"url(/fonts/poppins/poppins-regular.woff) format('woff'), url(/fonts/poppins/poppins-regular.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"italic",fontWeight:400,src:"url(/fonts/poppins/poppins-italic.woff) format('woff'), url(/fonts/poppins/poppins-italic.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:600,src:"url(/fonts/poppins/poppins-semi-bold.woff) format('woff'), url(/fonts/poppins/poppins-semi-bold.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:700,src:"url(/fonts/poppins/poppins-bold.woff) format('woff'), url(/fonts/poppins/poppins-bold.woff2) format('woff2')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"normal",fontWeight:400,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Regular.otf) format('opentype')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"normal",fontWeight:700,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Bold.otf) format('opentype')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"italic",fontWeight:400,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Italic.otf) format('opentype')"}}],Y=()=>i.jsx(d,{styles:[...B]}),Xo=[K({themes:{Default:h,Greenlane:O,TruckAPI:F},defaultTheme:"Default",Provider:A,GlobalStyles:Y})],Zo={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},viewMode:"docs",previewTabs:{"storybook/docs/panel":{index:-1},canvas:{title:"Sandbox"}},options:{storySort:{order:["Contribution Guidelines","Foundations","*"]}},docs:{theme:P.light}},tags:["autodocs"]};export{Xo as decorators,Zo as default};
