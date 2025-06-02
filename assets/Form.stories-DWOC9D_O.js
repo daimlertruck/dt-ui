550871a2ac4cb500a70c458a7201e8ef8d30cea9
dt-ui <dt-ui@daimlertruck.com>
2025-06-02 16:19:42 +0000

    chore: update and publish Storybook

---
+++ assets/Form.stories-DWOC9D_O.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-DMAvRu52.js";import{T as l}from"./TextField-Dkc2Tt_b.js";import"./TextField.styled-D_btMrKR.js";import{F as t}from"./Form-TjH1qBRq.js";import"./index-Dl6G-zuu.js";import"./Icon-B8gBCtaI.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./LabelField-C7Q__U6r.js";import"./Typography-DmCFU9KH.js";import"./index-B3tNC0y0.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-eZoG7xmy.js";import"./Tooltip-Cw8sbJn_.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Info-BRKcLzcc.js";const w={title:"Data Display/Form",component:t},u=()=>r.jsx(t,{children:r.jsx(l,{label:"label 1"})}),e=u.bind({});e.args={};const F=({isDisabled:d,title:c,tooltip:b})=>r.jsxs(t,{children:[r.jsx(t.Group,{title:c,tooltip:b,children:r.jsx(l,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:d,children:[r.jsx(l,{label:"label 2"}),r.jsx(l,{label:"label 3"})]})]}),o=F.bind({});o.args={title:"",tooltip:"",isDisabled:!1};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <Form>
+      <TextField label='label 1' />
+    </Form>;
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var m,p,n;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  isDisabled,
+  title,
+  tooltip
+}) => <Form>
+    <Form.Group title={title} tooltip={tooltip}>
+      <TextField label='label 1' />
+    </Form.Group>
+    <Form.Group isDisabled={isDisabled}>
+      <TextField label='label 2' />
+      <TextField label='label 3' />
+    </Form.Group>
+  </Form>`,...(n=(p=o.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const z=["Default","FormGroup"];export{e as Default,o as FormGroup,z as __namedExportsOrder,w as default};
