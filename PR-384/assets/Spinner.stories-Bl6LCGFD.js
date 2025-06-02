aa7579dcdbdba983c864a77b1e63fe45593b6eaf
dt-ui <dt-ui@daimlertruck.com>
2025-06-02 16:15:49 +0000

    chore: delete PR preview

--- PR-384/assets/Spinner.stories-Bl6LCGFD.js
+++
@@ -1,7 +0,0 @@
-import{S as a}from"./Spinner-BPP59PQ_.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./index-LNaMzCj1.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./Typography-B1KRbKKY.js";import"./index-53BALPLO.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-Shb4gMzs.js";const S={title:"Data Display/Spinner",component:a,parameters:{backgrounds:{values:[{name:"black",value:"#000"}]}},argTypes:{size:{options:["extra-small","small","medium","large","extra-large"],control:{type:"select"}},colorScheme:{options:["positive","negative"],control:{type:"radio"}}}},e={args:{size:"medium",colorScheme:"positive",loadingText:""}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 'medium',
-    colorScheme: 'positive',
-    loadingText: ''
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,S as default};
