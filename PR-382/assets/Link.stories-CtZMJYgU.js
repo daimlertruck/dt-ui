281f6681e650c92be812437bea6fff3f3508dd76
dt-ui <dt-ui@daimlertruck.com>
2025-06-02 09:56:24 +0000

    chore: update and publish Storybook

---
+++ PR-382/assets/Link.stories-CtZMJYgU.js
@@ -0,0 +1,10 @@
+import{j as a}from"./jsx-runtime-DMAvRu52.js";import{c as n}from"./codes-CmbLmPPF.js";import{L as t,a as l}from"./enum-B8z5HCNT.js";import"./index-Dl6G-zuu.js";import"./Icon-B8gBCtaI.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";const c=["small","medium","large"],g={component:t,title:"Data Display/Link",argTypes:{children:{control:"text"},target:{control:"text"},href:{control:"text"},textSize:{options:c,control:{type:"select"},description:"Sets the size of text"},isDisabled:{control:{type:"boolean"},defaultValue:{summary:!1},description:"Sets the link disabled"},variant:{options:["primary","secondary"],control:{type:"select"},description:"Sets the link variant"},icon:{options:Object.keys(n),control:{type:"select"},description:"Adds an Icon to the link"}},render:({...s})=>a.jsx(t,{...s})},e={args:{children:"Redirect to",href:"https://www.google.com/",target:"_blank",textSize:l.Medium,isDisabled:!1,variant:"primary"}};var r,o,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Redirect to',
+    href: 'https://www.google.com/',
+    target: '_blank',
+    textSize: LinkTextSize.Medium,
+    isDisabled: false,
+    variant: 'primary'
+  }
+}`,...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,g as default};
