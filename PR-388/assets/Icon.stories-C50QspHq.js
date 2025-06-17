aa87fb4094fbeeafbb73094e18cc451eed126957
dt-ui <dt-ui@daimlertruck.com>
2025-06-17 09:09:21 +0000

    chore: delete PR preview

--- PR-388/assets/Icon.stories-C50QspHq.js
+++
@@ -1,8 +0,0 @@
-import{c as t}from"./codes-CmbLmPPF.js";import{I as n}from"./Icon-B8gBCtaI.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";const m={title:"Data Display/Icon",component:n,argTypes:{code:{options:Object.keys(t),control:{type:"select"}},variant:{options:["filled","outlined"],control:{type:"radio"}},size:{options:["small","medium","large"],control:{type:"radio"}}},parameters:{controls:{matchers:{color:/(color)$/i}}}},o={args:{code:"warning",size:"large",variant:"outlined",color:"black"}};var r,e,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    code: 'warning',
-    size: 'large',
-    variant: 'outlined',
-    color: 'black'
-  }
-}`,...(a=(e=o.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const d=["Default"];export{o as Default,d as __namedExportsOrder,m as default};
