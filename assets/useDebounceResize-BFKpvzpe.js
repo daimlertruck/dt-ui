5410b4a56579f4971de67c39e18e25bf28b3e8fe
dt-ui <dt-ui@daimlertruck.com>
2025-06-25 16:04:36 +0000

    chore: update and publish Storybook

---
+++ assets/useDebounceResize-BFKpvzpe.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-Dl6G-zuu.js";import{d as i}from"./debounce-BF4Cdv7I.js";import"./jsx-runtime-DMAvRu52.js";import"./index-D_kfsQMV.js";import"./index-DCrStp1_.js";function p(o,r=500){t.useEffect(()=>{const e=i(o,r);return e(),window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)},[o,r])}export{p as u};
