38b16df2679d90fc9f994c099296b07c2afdc5ae
dt-ui <dt-ui@daimlertruck.com>
2025-07-03 12:06:01 +0000

    chore: update and publish Storybook

---
+++ PR-1/assets/Spinner-BWwcRhr0.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-DMAvRu52.js";import{B as m}from"./index-LNaMzCj1.js";import{T as d}from"./Typography-6BH9iM4L.js";import{k as x}from"./index-C7tVgOD1.js";import{n as c,u}from"./emotion-styled.browser.esm-DpDeK1WQ.js";const g=x`
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`,s={"extra-small":"14px",small:"28px",medium:"42px",large:"50px","extra-large":"58px"},i={"extra-small":"2px",small:"4px",medium:"4px",large:"4px","extra-large":"4px"},p=t=>({positive:{main:t.palette.primary.default,background:t.palette.content.contrast},negative:{main:t.palette.content.contrast,background:t.palette.content.medium}}),y=c.div`
+  ${({theme:t,size:n,colorScheme:r})=>`
+    width: ${s[n]};
+    height: ${s[n]};
+    border: ${i[n]} solid ${p(t)[r].background};
+    border-radius: 50%;
+    border-top: ${i[n]} solid ${p(t)[r].main};
+  `};
+  animation: ${g} 1s cubic-bezier(0.3, 0.1, 0.25, 0.65) 0s
+    infinite normal none;
+`,b={positive:"content.default",negative:"content.contrast"},f={"extra-small":"body4",small:"body4",medium:"body3",large:"body3","extra-large":"body2"},T=({style:t,dataTestId:n="spinner",size:r="medium",colorScheme:o="positive",loadingText:a})=>{const l=u();return e.jsxs(m,{dataTestId:n,style:{...t},children:[e.jsx(y,{colorScheme:o,size:r}),a?e.jsx(d,{color:b[o],fontStyles:f[r],style:{marginTop:l.spacing["3xs"]},children:a}):null]})};export{T as S};
