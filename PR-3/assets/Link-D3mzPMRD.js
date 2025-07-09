a522cc78c1f1b062970e33941dddb68cec4f0c72
dt-ui <dt-ui@daimlertruck.com>
2025-07-09 11:22:19 +0000

    chore: update and publish Storybook

---
+++ PR-3/assets/Link-D3mzPMRD.js
@@ -0,0 +1,41 @@
+import{j as n}from"./jsx-runtime-DMAvRu52.js";import{I as $}from"./Icon-Cz77WqqY.js";import{r as f}from"./index-Dl6G-zuu.js";import{n as x}from"./emotion-styled.browser.esm-DpDeK1WQ.js";const u=x.a`
+  ${({theme:o,disabled:r,size:t,variant:l="primary"})=>`
+    color: ${o.palette[l].default};
+    text-decoration: none;
+    display: inline-flex;
+    align-items: center;
+    gap: ${o.spacing["5xs"]};
+
+    ${r&&`
+        color: ${o.palette[l].light};
+        cursor: not-allowed;
+      `};
+
+    ${!r&&`
+        &:hover > span {
+          text-decoration: underline;
+        }
+
+        &:active {
+          color: ${o.palette[l].dark};
+          text-decoration: none;
+        }
+      `};
+
+    &:focus-visible {
+      outline: 2px solid ${o.palette.primary.default};
+    }
+
+    ${t=="large"&&`
+        ${o.fontStyles.body1Bold}
+      `};
+
+    ${t=="medium"&&`
+        ${o.fontStyles.body2Bold}
+      `};
+
+    ${t=="small"&&`
+        ${o.fontStyles.body3Bold}
+      `};
+  `}
+`,S=f.forwardRef(({isDisabled:o,children:r,size:t="medium",dataTestId:l,onClick:a,variant:d,href:i,style:s,icon:e,...p},c)=>n.jsxs(u,{"data-testid":l??"link",disabled:o,ref:c,size:t,style:s,variant:d,...!o&&{href:i,onClick:a},...p,children:[n.jsx("span",{children:r}),e?n.jsx($,{code:e,color:"inherit",size:t}):null]}));export{S as L};
---
+++ PR-3/assets/Select.stories-CtLLzg9B.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-DMAvRu52.js";import{S as a}from"./Select-BSNlTYFB.js";import"./index-Dl6G-zuu.js";import"./index-C0TCctYE.js";import"./index-DCrStp1_.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-BAMY2Nnw.js";import"./iframe-Bgkg-D_w.js";import"./useClickOutside-CBJSS0tk.js";import"./Icon-Cz77WqqY.js";import"./LabelField-BwcgLvk-.js";import"./Typography-CHergP0E.js";import"./Tooltip-8YQKxENk.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Checkbox-CDOmFQAM.js";const d=[{value:"opt1",label:"Option 1",disabled:!1},{value:"opt2",label:"Option 2",disabled:!1},{value:"opt3",label:"A text message to see how big labels will be handled",disabled:!1},{value:"opt4",label:"Option 3",disabled:!0},{value:"opt5",label:"Option 4",disabled:!1}],T={title:"Data Display/Select",component:a,argTypes:{helperText:{control:"text"},variant:{options:["outlined","bottom-line"],control:{type:"select"}},fill:{options:["default","contrast","light"],control:{type:"select"}}},render:({options:r,...p})=>t.jsx("div",{style:{height:"500px",padding:"10px"},children:t.jsx(a,{...p,children:r.map((e,n)=>t.jsx(a.Option,{disabled:e.disabled,index:n,label:e.label,value:e.value},e.value))})})},l={args:{options:d,label:"My label",helperText:"",errorMessage:"",isMulti:!1,isRequired:!1,hasError:!1,isDisabled:!1,initialValue:""}};var s,o,i;l.parameters={...l.parameters,docs:{...(s=l.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    options: options,
+    label: 'My label',
+    helperText: '',
+    errorMessage: '',
+    isMulti: false,
+    isRequired: false,
+    hasError: false,
+    isDisabled: false,
+    initialValue: ''
+  }
+}`,...(i=(o=l.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const R=["Default"];export{l as Default,R as __namedExportsOrder,T as default};
