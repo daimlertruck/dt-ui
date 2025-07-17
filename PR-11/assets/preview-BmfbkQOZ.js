b508e27324b6ee4a9c6ec6a44c9d61c56aff832f
dt-ui <dt-ui@daimlertruck.com>
2025-07-17 17:14:48 +0000

    chore: update and publish Storybook

---
+++ PR-11/assets/preview-BmfbkQOZ.js
@@ -0,0 +1,33 @@
+import{j as i}from"./jsx-runtime-DMAvRu52.js";import{e as _,g as S,G as d}from"./index-p3766LgV.js";import{_ as P}from"./index-BOnZ8sI7.js";import{d as R}from"./index-DrFu-skq.js";import{R as r}from"./index-Dl6G-zuu.js";import{t as h}from"./index-tKEb1WUz.js";import{T as x}from"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./index-DCrStp1_.js";import{a as F,t as O}from"./index-DFUtGYHP.js";import"./index-BAMY2Nnw.js";import"./iframe-D-QSvPbA.js";import"./AccordionBody.styled-BsWtaJvH.js";import"./Icon-BZQueZez.js";import"./Tooltip-D0PUH9A3.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Avatar.styled-Xq2p-nwB.js";import"./Backdrop-XIJ9Z3lL.js";import"./index-LNaMzCj1.js";import"./Breadcrumb-g4Gz0gDH.js";import"./useMedia-D3XZz7EL.js";import"./useDebounceResize-BpHcYWqy.js";import"./debounce-BF4Cdv7I.js";import"./Divider.styled-Cm1mEvej.js";import"./Drawer-DmyhgjUp.js";import"./Typography-DCXQIPPK.js";import"./IconButton-k4gaNOO5.js";import"./Close-mVzWqPf0.js";import"./EmptyState-DaFO8M8h.js";import"./LabelField-RsoIAotz.js";import"./Message-BqusbCEj.js";import"./Info-BRKcLzcc.js";import"./Spinner-lyP5OknH.js";import"./Tabs-C7Ge8ymH.js";import"./Tag-CdildJjo.js";import"./index-CA9tWgwn.js";import"./Checkbox-BQ7mDs7U.js";import"./TextField.styled-D_btMrKR.js";import"./Table-Bn3ldYTA.js";import"./Button.styled-BUWpBxv_.js";import"./ProgressBar.styled-m8XzoYja.js";import"./Select-CZA-2b7d.js";import"./useClickOutside-CBJSS0tk.js";import"./Step-DsJ6D4nw.js";import"./Radio.styled-yyYnSofk.js";import"./Modal-BiRVZ0rb.js";import"./Card-BOJuIP8d.js";import"./SegmentedControl-C7q6fdm6.js";import"./Dropdown-iAmm4r5U.js";import"./Toggle.styled-uavW2Zwj.js";import"./TextArea.styled-DlXVwu5K.js";import"./Form-xS09IcMU.js";import"./Pagination-C3TIFYKx.js";import"./constants-CmWk560j.js";const D=o=>S`
+  ${_}
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: greyscale;
+    font-family: ${o.fontFamily.default};
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${o.fontFamily.default};
+    width: 100%;
+    min-height: 100vh;
+
+    ${o.fontStyles.body1}
+  }
+
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
+`,A=({theme:o=h,webFonts:t=[],children:e})=>i.jsxs(i.Fragment,{children:[i.jsx(d,{styles:[D(o),...t]}),i.jsx(x,{theme:o,children:e})]}),{useParameter:C,addons:G,useEffect:Vo,useMemo:I,definePreview:Jo}=__STORYBOOK_MODULE_PREVIEW_API__,{deprecate:M}=__STORYBOOK_MODULE_CLIENT_LOGGER__;var W=Object.defineProperty,b=(o,t)=>{for(var e in t)W(o,e,{get:t[e],enumerable:!0})},k={};b(k,{initialGlobals:()=>L});var n="themes",j=`storybook/${n}`,y="theme",u={},$={REGISTER_THEMES:`${j}/REGISTER_THEMES`},L={[y]:""},N={};b(N,{initializeThemeState:()=>g,pluckThemeFromContext:()=>w,useThemeParameters:()=>z});function w({globals:o}){return o[y]||""}function z(o){return M(R`The useThemeParameters function is deprecated. Please access parameters via the context directly instead e.g.
+    - const { themeOverride } = context.parameters.themes ?? {};
+    `),o?o.parameters[n]??u:C(n,u)}function g(o,t){G.getChannel().emit($.REGISTER_THEMES,{defaultTheme:t,themes:o})}var H=([o,t])=>t,K=({Provider:o,GlobalStyles:t,defaultTheme:e,themes:a={}})=>{let E=Object.keys(a),f=e;return g(E,f),(m,p)=>{let{themeOverride:s}=p.parameters[n]??{},l=w(p),T=I(()=>{let v=s||l||f,c=Object.entries(a);return c.length===1?H(c[0]):a[v]},[l,s]);return o?r.createElement(o,{theme:T},t&&r.createElement(t,null),m()):r.createElement(r.Fragment,null,t&&r.createElement(t,null),m())}};const B=[{"@font-face":{fontFamily:"Roboto",fontStyle:"normal",fontWeight:400,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-regular.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-regular.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Roboto",fontStyle:"italic",fontWeight:400,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Roboto",fontStyle:"normal",fontWeight:900,src:"url(/fonts/roboto-v30-latin/roboto-v30-latin-900.woff) format('woff'), url(/fonts/roboto-v30-latin/roboto-v30-latin-900.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:400,src:"url(/fonts/poppins/poppins-regular.woff) format('woff'), url(/fonts/poppins/poppins-regular.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"italic",fontWeight:400,src:"url(/fonts/poppins/poppins-italic.woff) format('woff'), url(/fonts/poppins/poppins-italic.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:600,src:"url(/fonts/poppins/poppins-semi-bold.woff) format('woff'), url(/fonts/poppins/poppins-semi-bold.woff2) format('woff2')"}},{"@font-face":{fontFamily:"Poppins",fontStyle:"normal",fontWeight:700,src:"url(/fonts/poppins/poppins-bold.woff) format('woff'), url(/fonts/poppins/poppins-bold.woff2) format('woff2')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"normal",fontWeight:400,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Regular.otf) format('opentype')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"normal",fontWeight:700,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Bold.otf) format('opentype')"}},{"@font-face":{fontFamily:"DaimlerCSPro",fontStyle:"italic",fontWeight:400,src:"url(/fonts/daimler-cs-pro/DaimlerCSPro-Italic.otf) format('opentype')"}}],Y=()=>i.jsx(d,{styles:[...B]}),Xo=[K({themes:{Default:h,Greenlane:O,TruckAPI:F},defaultTheme:"Default",Provider:A,GlobalStyles:Y})],Zo={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},viewMode:"docs",previewTabs:{"storybook/docs/panel":{index:-1},canvas:{title:"Sandbox"}},options:{storySort:{order:["Contribution Guidelines","Foundations","*"]}},docs:{theme:P.light}},tags:["autodocs"]};export{Xo as decorators,Zo as default};
