1f1510e076ad3ff574682c6a0b20d9d8233e1940
dt-ui <dt-ui@daimlertruck.com>
2025-07-21 09:33:20 +0000

    chore: cleanup unused folders

--- PR-8/assets/preview-BCKez0ng.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./index-BtPb3vPd.js";import"./iframe-DosofAfS.js";import"./index-BAMY2Nnw.js";var n=r({step:(p,t,e)=>t(e)},{intercept:!0}).step,s={throwPlayFunctionExceptions:!1};export{s as parameters,n as runStep};
