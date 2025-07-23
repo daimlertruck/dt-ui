39e9d7fc46c9acc991cefc1c4ca502be053bff7f
dt-ui <dt-ui@daimlertruck.com>
2025-07-23 23:57:04 +0000

    chore: update and publish Storybook

---
+++ PR-17/assets/Form.stories-Czvwkzxd.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-DMAvRu52.js";import{T as l}from"./TextField-Dtht7P51.js";import"./TextField.styled-DhE2RjEO.js";import{F as t}from"./Form-B86R-hhx.js";import"./index-Dl6G-zuu.js";import"./Icon-D93abFDa.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./IconButton-k4gaNOO5.js";import"./LabelField-CEXKsqVl.js";import"./Typography-DGF3aCS1.js";import"./index-Qm_yRYDH.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-CGh7h3dv.js";import"./Tooltip-Cjzw78Iq.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Info-BRKcLzcc.js";const z={title:"Data Display/Form",component:t},u=()=>r.jsx(t,{children:r.jsx(l,{label:"label 1"})}),o=u.bind({});o.args={};const F=({isDisabled:d,title:c,tooltip:b})=>r.jsxs(t,{children:[r.jsx(t.Group,{title:c,tooltip:b,children:r.jsx(l,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:d,children:[r.jsx(l,{label:"label 2"}),r.jsx(l,{label:"label 3"})]})]}),e=F.bind({});e.args={title:"",tooltip:"",isDisabled:!1};var s,a,i;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <Form>
+      <TextField label='label 1' />
+    </Form>;
+}`,...(i=(a=o.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var m,p,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  isDisabled,
+  title,
+  tooltip
+}) => <Form>
+    <Form.Group title={title} tooltip={tooltip}>
+      <TextField label='label 1' />
+    </Form.Group>
+    <Form.Group isDisabled={isDisabled}>
+      <TextField label='label 2' />
+      <TextField label='label 3' />
+    </Form.Group>
+  </Form>`,...(n=(p=e.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const A=["Default","FormGroup"];export{o as Default,e as FormGroup,A as __namedExportsOrder,z as default};
