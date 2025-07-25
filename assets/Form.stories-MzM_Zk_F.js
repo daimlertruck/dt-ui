87f898e3ddce8f60bf0dc1ab3d931bf5feb36c07
dt-ui <dt-ui@daimlertruck.com>
2025-07-25 18:19:39 +0000

    chore: update and publish Storybook

---
+++ assets/Form.stories-MzM_Zk_F.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-DMAvRu52.js";import{T as l}from"./TextField-BCHp_Vgi.js";import"./TextField.styled-DhE2RjEO.js";import{F as t}from"./Form-BKMp6EvC.js";import"./index-Dl6G-zuu.js";import"./Icon-DJuXZjHV.js";import"./emotion-styled.browser.esm-DpDeK1WQ.js";import"./IconButton-k4gaNOO5.js";import"./LabelField-BQ4qdQ0T.js";import"./Typography-DVK4QH_x.js";import"./index-DgepbwHu.js";import"./index-DCrStp1_.js";import"./index-BAMY2Nnw.js";import"./iframe-BJhqu2ed.js";import"./Tooltip-P4BK6RIz.js";import"./Portal-DcNaNmD6.js";import"./z-index-Ds5QkyqS.js";import"./Info-BRKcLzcc.js";const z={title:"Data Display/Form",component:t},u=()=>r.jsx(t,{children:r.jsx(l,{label:"label 1"})}),o=u.bind({});o.args={};const F=({isDisabled:d,title:c,tooltip:b})=>r.jsxs(t,{children:[r.jsx(t.Group,{title:c,tooltip:b,children:r.jsx(l,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:d,children:[r.jsx(l,{label:"label 2"}),r.jsx(l,{label:"label 3"})]})]}),e=F.bind({});e.args={title:"",tooltip:"",isDisabled:!1};var s,a,i;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <Form>
+      <TextField label='label 1' />
+    </Form>;
+}`,...(i=(a=o.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var m,p,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  isDisabled,
+  title,
+  tooltip
+}) => <Form>
+    <Form.Group title={title} tooltip={tooltip}>
+      <TextField label='label 1' />
+    </Form.Group>
+    <Form.Group isDisabled={isDisabled}>
+      <TextField label='label 2' />
+      <TextField label='label 3' />
+    </Form.Group>
+  </Form>`,...(n=(p=e.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const A=["Default","FormGroup"];export{o as Default,e as FormGroup,A as __namedExportsOrder,z as default};
